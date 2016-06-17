887f4c542a4fad6d778c2ca6dba6abb64a00a441
bhbraswell <rbraswell@appliedgeosolutions.com>
2016-06-17 14:10:24 -0400

    talking about 'where' made me want to add a test using where

--- gips/test/modis.sh
+++ gips/test/modis.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 ARGS="-s /etc/gips/test/NHseacoast.shp -d 2012-12-01,2012-12-10 -v 4"
+ARGS2="-s /etc/gips/test/nsamerica.shp --where "CNTRY_NAME='Guyana'" -d 2012-12-01 -v 4"
+
 
 gips_info Modis
 gips_inventory Modis $ARGS --fetch
@@ -9,6 +11,10 @@ gips_process Modis $ARGS
 # mosaic
 gips_project Modis $ARGS --res 100 100 --outdir modis_project --notld
 gips_stats modis_project/*
+
+# mosaic selected location
+gips_project Modis $ARGS --res 1000 1000 --outdir modis_project_loc --notld
+gips_stats modis_project_loc/*
 
 # mosaic without warping
 gips_project Modis $ARGS --outdir modis_project_nowarp --notld
