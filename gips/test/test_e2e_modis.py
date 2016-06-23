6ea2849479b537c1842261fcce8f28011c974fe7
Tom Olson <tolson@appliedgeosolutions.com>
2016-06-23 09:08:18 -0400

    Verify output files against known good list

--- gips/test/test_e2e_modis.py
+++ gips/test/test_e2e_modis.py
@@ -55,18 +55,56 @@ def test_file_environment():
     # TODO when does this step get skipped?  During some errors it seems?
     gtfe.remove_created()
 
+expected_files = set([os.path.join('modis/tiles/h12v04/', f) for f in [
+    '2012337/h12v04_2012337_MCD_quality.tif',
+    '2012337/h12v04_2012337_MOD_temp8td.tif',
+    '2012337/h12v04_2012337_MCD_snow.tif',
+    '2012337/h12v04_2012337_MCD_fsnow.tif',
+    '2012337/MYD11A1.A2012337.h12v04.005.2012341072847.hdf.index',
+    '2012337/MCD43A2.A2012337.h12v04.005.2012356160504.hdf.index',
+    '2012337/MOD09Q1.A2012337.h12v04.005.2012346141041.hdf.index',
+    '2012337/h12v04_2012337_MOD_temp8tn.tif',
+    '2012337/MOD11A1.A2012337.h12v04.005.2012339204007.hdf.index',
+    '2012337/h12v04_2012337_MOD_clouds.tif',
+    '2012337/MYD10A1.A2012337.h12v04.005.2012340112013.hdf.index',
+    '2012337/MOD11A2.A2012337.h12v04.005.2012346152330.hdf.index',
+    '2012337/h12v04_2012337_MOD-MYD_obstime.tif',
+    '2012337/h12v04_2012337_MOD_ndvi8.tif',
+    '2012337/MCD43A4.A2012337.h12v04.005.2012356160504.hdf.index',
+    '2012337/MOD10A1.A2012337.h12v04.005.2012340033542.hdf.index',
+    '2012337/h12v04_2012337_MOD-MYD_temp.tif',
+    '2012337/h12v04_2012337_MCD_indices.tif',
+    '2012336/MOD11A1.A2012336.h12v04.005.2012339180517.hdf.index',
+    '2012336/MYD11A1.A2012336.h12v04.005.2012341040543.hdf.index',
+    '2012336/h12v04_2012336_MCD_fsnow.tif',
+    '2012336/h12v04_2012336_MOD_clouds.tif',
+    '2012336/h12v04_2012336_MOD-MYD_obstime.tif',
+    '2012336/MYD10A1.A2012336.h12v04.005.2012340031954.hdf.index',
+    '2012336/h12v04_2012336_MOD-MYD_temp.tif',
+    '2012336/h12v04_2012336_MCD_snow.tif',
+    '2012336/MOD10A1.A2012336.h12v04.005.2012339213007.hdf.index',
+    '2012338/MYD10A1.A2012338.h12v04.005.2012340142152.hdf.index',
+    '2012338/h12v04_2012338_MOD-MYD_temp.tif',
+    '2012338/MYD11A1.A2012338.h12v04.005.2012341075802.hdf.index',
+    '2012338/h12v04_2012338_MCD_fsnow.tif',
+    '2012338/h12v04_2012338_MCD_snow.tif',
+    '2012338/MOD10A1.A2012338.h12v04.005.2012341091201.hdf.index',
+    '2012338/h12v04_2012338_MOD_clouds.tif',
+    '2012338/MOD11A1.A2012338.h12v04.005.2012341041222.hdf.index',
+    '2012338/h12v04_2012338_MOD-MYD_obstime.tif',
+]])
+
 def test_e2e_process(setup_modis_data, test_file_environment):
     """Test gips_process on modis data."""
     logger.info('starting run')
     # TODO why is expect_error there?
     # TODO better error reporting when this process fails (break path to
     # shapefile to reproduce problem)
+    # TODO how do I make gips_process output appear on the console in real time?
     outcome = test_file_environment.run('gips_process', *STD_ARGS,
                                         expect_error=True)
     logger.info('run complete')
 
     # confirm files created but not deleted;
-    # files_updated includes dirs whose contents have changed
-    assert (bool(outcome.files_created)
-            and bool(outcome.files_updated)
-            and not bool(outcome.files_deleted))
+    assert (expected_files == set(outcome.files_created.keys())
+            and not outcome.files_deleted)
