6c28308ba9309253fec33f9dbf2fce5862189eda
Tom Olson <tolson@appliedgeosolutions.com>
2016-07-13 13:48:05 -0400

    57 sys test updates to handle issue

--- gips/test/sys/t_repo_src_alteration.py
+++ gips/test/sys/t_repo_src_alteration.py
@@ -1,4 +1,5 @@
 import logging
+from datetime import datetime
 
 import pytest
 
@@ -25,11 +26,31 @@ def careful_repo_env(request, expected):
 src_altering = pytest.mark.skipif(not pytest.config.getoption("src_altering"),
                                   reason="--src-altering is required for this test")
 
+is_wednesday = datetime.today().date().weekday() == 2
+
 @src_altering
+@pytest.mark.skipif(is_wednesday, reason="Data isn't available on Wednesdays")
 def t_modis_inv_fetch(careful_repo_env, expected):
     """Test gips_inventory --fetch; actually contacts data provider."""
     # only get data for one day to save time
     args = ('modis', '-s', NH_SHP_PATH,
             '-d', '2012-12-01,2012-12-01', '-v', '4', '--fetch')
     actual = careful_repo_env.run('gips_inventory', *args)
+    assert expected == actual
+
+
+@pytest.mark.timeout(20)
+@pytest.mark.skipif(not is_wednesday, reason="This test is only meaningful on Wednesdays")
+# TODO use careful version once expectations are known:
+# def t_modis_inv_fetch_on_wednesday(careful_repo_env):
+def t_modis_inv_fetch_on_wednesday(repo_env):
+    """Test gips_inventory --fetch's graceful failure during planned outages.
+
+    Actually contacts data provider, but only on Wednesdays."""
+    # only get data for one day to save time
+    args = ('modis', '-s', NH_SHP_PATH,
+            '-d', '2012-12-01,2012-12-01', '-v', '4', '--fetch')
+    # TODO some files may be generated; put those in expected/ when they've been identified
+    expected = GipsProcResult()
+    actual = repo_env.run('gips_inventory', *args)
     assert expected == actual
