19e1137460821f58a35524b6d060030fb0eeea03
Tom Olson <tom.olson@gmail.com>
2016-12-14 13:48:34 -0500

    proto: int test for process worker function
    Known to be good against mocked process function.

--- gips/test/int/t_datahandler.py
+++ gips/test/int/t_datahandler.py
@@ -26,12 +26,12 @@ def t_fetch(status_table):
     gippy.Options.SetVerbose(4) # substantial verbosity for testing purposes
 
     os.environ['GIPS_ORM'] = 'true'
-    key_kwargs = dict(driver='modis', 
-                      asset ='MCD43A2', 
+    key_kwargs = dict(driver='modis',
+                      asset ='MCD43A2',
                       tile  ='h12v04',
                       date  =datetime.date(2012, 12, 1))
     fetch_args = tuple(key_kwargs[k] for k in ('driver', 'asset', 'tile', 'date'))
-    api_kwargs = dict(sensor='dontcare', 
+    api_kwargs = dict(sensor='dontcare',
                       name  ='unspecified',
                       status='requested',
                       **key_kwargs)
@@ -52,3 +52,53 @@ def t_fetch(status_table):
     assert (expected == returned_asset.name == queried_asset.name
             and 'complete' == returned_asset.status.status == queried_asset.status.status
             and os.path.isfile(expected))
+
+
+@pytest.mark.django_db()
+def t_process(status_table):
+    """Simple test for worker.process.  Warning:  Operates destructively."""
+    from gips.datahandler.worker import process
+    import gippy
+
+    # check dependency:  if it's not there the test fails (to get it run the prior fetch test)
+    dependee_fname = os.path.join(
+            util.DATA_REPO_ROOT,
+            'modis/tiles/h12v04/2012336/MCD43A2.A2012336.h12v04.006.2016112010833.hdf')
+    assert os.path.isfile(dependee_fname), "Couldn't satisfy asset dependency, giving up"
+    dependee_model = dbinv.models.Asset(driver='modis',     asset ='MCD43A2',
+                                        tile  ='h12v04',    date  =datetime.date(2012, 12, 1),
+                                        sensor='dontcare',  name  =dependee_fname,
+                                        status=dbinv.api._status('complete'))
+    dependee_model.save()
+
+    # config
+    gippy.Options.SetVerbose(4) # substantial verbosity for testing purposes
+
+    os.environ['GIPS_ORM'] = 'true'
+    key_kwargs = dict(driver ='modis',
+                      product='quality',
+                      tile   ='h12v04',
+                      date   =datetime.date(2012, 12, 1))
+    process_args = tuple(key_kwargs[k] for k in ('driver', 'product', 'tile', 'date'))
+    api_kwargs = dict(sensor='dontcare',
+                      name  ='unspecified',
+                      status='requested',
+                      **key_kwargs)
+    fname = os.path.join(
+            util.DATA_REPO_ROOT,
+            'modis/tiles/h12v04/2012336/h12v04_2012336_MCD_quality.tif')
+
+    # setup
+    orm.setup()
+    if os.path.exists(fname): # remove the prod if it's present
+        os.remove(fname)
+    dbinv.update_or_add_product(**api_kwargs) # set the prod to 'requested' status
+
+    # test
+    returned_prod = process(*process_args)
+    queried_prod = dbinv.models.Product.objects.get(**key_kwargs)
+
+    expected = {'fname': fname,              'status': 'complete'}
+    returned = {'fname': returned_prod.name, 'status': returned_prod.status.status}
+    queried  = {'fname': queried_prod.name,  'status': queried_prod.status.status}
+    assert (expected == returned == queried) and os.path.isfile(fname)
