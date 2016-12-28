3ab8d3b24c2a3ad5dcd5c7caa54d94f0c97d94d1
Bobby Braswell <rob.braswell@unh.edu>
2016-12-28 20:55:49 +0000

    can convert to reflectance

--- gips/data/aster/convert_L1T.py
+++ gips/data/aster/convert_L1T.py
@@ -1,92 +1,10 @@
 # -*- coding: utf-8 -*-
+
+""" 
+Based on code provided by NASA
+See docstring in docs/ASTERL1T_DN2REF.py
 """
--------------------------------------------------------------------------------
- Open ASTER L1T HDF-EOS and Export as Geotiff Tool
- How To Tutorial
- This tool imports ASTER L1T HDF-EOS files, georeferences, and  exports files 
- as geotiffs.
--------------------------------------------------------------------------------
- Author: Cole Krehbiel
- Contact: LPDAAC@usgs.gov  
- Organization: Land Processes Distributed Active Archive Center
- Date last modified: 8-25-2016
--------------------------------------------------------------------------------
- OBJECTIVE:
- The Advanced Spaceborne Thermal Emission and Reflection Radiometer (ASTER) 
- Level 1 Precision Terrain Corrected Registered At-Sensor Radiance  Dataset 
- (AST_L1T) provides calibrated and geometrically corrected at-sensor radiance 
- for ASTER VNIR (bands 1-3N), SWIR (bands 4-9), and TIR (bands 10-14). 
- ASTER L1T files can be downloaded from the Datapool in HDF-EOS format (.hdf). 
- ASTER L1T is derived from ASTER Level 1A data that has been geometrically 
- corrected and reprojected to a north-up UTM projection. However, when brought
- into GIS or Remote Sensing software programs such as ArcMap or QGIS, the data
- appears to be missing a correctly defined coordinate reference system (CRS). 
- This tutorial demonstrates how a Python script can be used to open ASTER L1T 
- HDF-EOS files, correctly define the CRS, and export each VNIR/SWIR/TIR band
- (14 total) as a GeoTiff that can be loaded with spatial reference into the
- aforementioned GIS/Remote Sensing software programs.
 
- The tutorials take an ASTER L1T HDF-EOS file (.hdf) as an input and outputs 
- georeferenced tagged image file format (GeoTiff) files for each of the VNIR,
- SWIR, and TIR science datasets contained in the original ASTER L1T file.
-
- Results from this tutorial are output in Universal Transverse Mercator (UTM)
- with WGS84 as GeoTiff files. The output GeoTiffs for each band include:
- 1. Original_AST_L1T_Filename_ImageDataband#.tif
-Data is At-Sensor radiance stored as DN.
-
- This tool was specifically developed for ASTER L1T HDF-EOS files and should 
- only be used for those data products.
--------------------------------------------------------------------------------
- PREREQUISITES
- Python software program version 3.4 or 2.7
- 
- Python Packages/Modules:
- osgeo with gdal and osr – 1.11.1 
- numpy – 1.11.0
- os
- glob
-datetime
-re
--------------------------------------------------------------------------------
- ESTIMATED TIME TO COMPLETE
- Time to complete processing for a single file has typically been < 1 minute 
-
- NOTE: Estimated completion times will vary by OS and/or machine specifications 
--------------------------------------------------------------------------------
- ADDITIONAL INFORMATION
- LP DAAC ASTER L1T Product Page: 
- https://lpdaac.usgs.gov/dataset_discovery/aster/aster_products_table/ast_l1t
- LP DAAC ASTER L1T User Guide: 
- https://lpdaac.usgs.gov/sites/default/files/public/product_documentation/
- aster_l1t_users_guide.pdf
-
- This tool will batch process ASTER L1T files if more than 1 is located in the
- working directory.
- Output file directory will be 'inputfiledirectory'+'/output/'
--------------------------------------------------------------------------------
- RELATED TUTORIALS
-  ASTERL1T_DN2REF.py --Converts ASTER L1T Radiance (stored as DN) to 
- Top of Atmosphere Reflectance
- 
- ASTERL1T_DN2REF.R --Converts ASTER L1T Radiance (stored as DN) to 
- Top of Atmosphere Reflectance
- 
- ASTERL1T_hdf2tif.R --Imports ASTER L1T HDF-EOS files, georeferences and
- exports as geotiffs (R equivalent of this script)
-
- Search for other tools at https://lpdaac.usgs.gov/
--------------------------------------------------------------------------------
- LABELS
- ASTER L1T, Georeference, GeoTiff, HDF-EOS, LP DAAC, Python
--------------------------------------------------------------------------------
-"""
-#------------------------------------------------------------------------------
-# PROCEDURES:
-#         IMPORTANT:
-# User needs to change the 'in_dir' (working directory) below, denoted by #***
-
-# Load necessary packages into Python
 from osgeo import gdal, osr
 import numpy as np
 import os
@@ -94,39 +12,8 @@ import glob
 from datetime import datetime
 import re
 
-
 from pdb import set_trace
 
-
-#------------------------------------------------------------------------------
-#|                    Batch Process files from directory:                     |
-#------------------------------------------------------------------------------
-
-# Create and/or set input and output directories
-
-in_dir = "/data/aster/test"
-out_dir = "/data/aster/test"
-
-file_list = ['AST_L1T_00303172004174949_20150503164011_38074.hdf']
-
-if not os.path.exists(out_dir):
-    os.makedirs(out_dir)
-        
-
-#------------------------------------------------------------------------------
-# Set up calculations
-# 1. DN to Radiance (Abrams, 1999)
-# Radiance = (DN-1)* Unit Conversion Coefficient
-
-# 2. Radiance to TOA Reflectance
-# Reflectance_TOA = (pi*Lrad*d2)/(esuni*COS(z))
-
-# Define the following:
-# Unit Conversion Coefficient = ucc
-# pi = pi
-# Radiance,Lrad  = rad
-# esuni = esun 
-# z = solare
 
 # Order for ucc (Abrams, 1999) is: Band 1 high, normal, low, low2; 
 # Band 2 h, n, l1, l2; b3 h, n, l1, l2 (3N & 3B the same) 
@@ -145,23 +32,9 @@ ucc = np.matrix(([[0.676, 1.688, 2.25, 0.0],\
 # Ordered b1, b2, b3N, b4, b5...b9
 irradiance = [1848, 1549, 1114, 225.4, 86.63, 81.85, 74.85, 66.49, 59.85]
 
-def dn2rad (x):
-    rad = (x-1.)*ucc1
-    return rad
 
-def rad2ref (rad):
-    ref = (np.pi * rad * (esd * esd)) / (irradiance1 * np.sin(np.pi * sza / 
-    180))
-    return ref
-
-#------------------------------------------------------------------------------
-# Loop through all ASTER L1T hdf files in the directory
-for k in range(len(file_list)):
+def convert(file_path):
     
-    # Maintains original filename convention    
-    file_name = file_list[k]    
-    file_path = os.path.join(in_dir, file_name)
-
     # Read in the file and metadata
     aster = gdal.Open(file_path)
     aster_sds = aster.GetSubDatasets()
@@ -212,7 +85,7 @@ for k in range(len(file_list)):
         utm_zone = utm_n[utm]
         
     del utm_n, utm_s, utm, meta
-#------------------------------------------------------------------------------   
+
     # Loop through all ASTER L1T SDS (bands)    
     for i in range(len(aster_sds)):
     
@@ -228,6 +101,7 @@ for k in range(len(file_list)):
             aster_sd3 = aster_sd2[1:-1]
             band = aster_sd3.split(':')[-1]
 
+            out_dir, file_name = os.path.split(file_path)
             base_filename = file_name.split('.hdf')[0] + '_' + band + '.tif'
             out_filename = os.path.join(out_dir, base_filename)
             out_filename_rad = out_filename.split('.tif')[0] + '_rad.tif'
@@ -263,7 +137,7 @@ for k in range(len(file_list)):
             # Define UL x and y coordinates based on spatial resolution           
             ul_yy = ul_y - (y_res/2)
             ul_xx = ul_x - (x_res/2)
-#------------------------------------------------------------------------------
+
             # Start conversions by band (1-9)        
             if band == 'ImageData1':
                 bn = -1 + 1                
@@ -354,16 +228,18 @@ for k in range(len(file_list)):
                     ucc1 = ucc[bn, 1] 
                 else:
                     ucc1 = ucc[bn, 2] 
-#------------------------------------------------------------------------------
+
             #Set irradiance value for specific band
             irradiance1 = irradiance[bn]
             
             # Convert from DN to Radiance        
-            rad = dn2rad(sds)
-            
+            #rad = dn2rad(sds)
+            rad = (sds-1.)*ucc1
+
             # Convert from Radiance to TOA Reflectance
-            ref = rad2ref(rad)
-         
+            #ref = rad2ref(rad)
+            ref = (np.pi * rad * (esd * esd)) / (irradiance1 * np.sin(np.pi * sza / 180.))
+
             # Generate output Geotiff files
             # First, Radiance (stored as DN)
             driver = gdal.GetDriverByName('GTiff')
@@ -408,25 +284,9 @@ for k in range(len(file_list)):
             
             del band, bn, gname, irradiance1, out_filename, \
             out_filename_rad, out_filename_ref, rad, ref, sds, ucc1
-#------------------------------------------------------------------------------
-# References
-# ABRAMS, M., HOOK, S., and RAMACHANDRAN, B., 1999, Aster user handbook,
-# Version 2, NASA/Jet Propulsion Laboratory, Pasadena, CA, at 
-# https://asterweb.jpl.nasa.gov/content/03_data/04_Documents/
-# aster_user_guide_v2.pdf
 
-# ARCHARD, F., AND D’SOUZA, G., 1994, Collection and pre-processing of 
-# NOAA-AVHRR 1km resolution data for tropical forest resource assessment. 
-# Report EUR 16055, European Commission, Luxembourg, at 
-# http://bookshop.europa.eu/en/collection-and-pre-processing-of-noaa-avhrr-1-km
-# -resolution-data-for-tropical-forest-resource-assessment-pbCLNA16055/
 
-# EVA, H., AND LAMBIN, E.F., 1998, Burnt area mapping in Central Africa using 
-# ATSR data, International Journal of Remote Sensing, v. 19, no. 18, 3473-3497, 
-# at http://dx.doi.org/10.1080/014311698213768
+if __name__ == "__main__":
 
-# Thome, K.J., Biggar, S.F., and Slater, P.N., 2001, Effects of assumed solar
-# spectral irradiance on intercomparisons of earth-observing sensors. In 
-# International Symposium on Remote Sensing, International Society for Optics
-# and Photonics, pp. 260-269, at http://dx.doi.org/10.1117/12.450668.
-#------------------------------------------------------------------------------
+    file_path  = os.path.join('/data/aster/test/AST_L1T_00303172004174949_20150503164011_38074.hdf')
+    convert(file_path)
