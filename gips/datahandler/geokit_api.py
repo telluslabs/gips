d437da8d3f631aa64f519e7e371c08ccb5f14cb5
Justin Fisk <jfisk@appliedgeosolutions.com>
2016-12-29 06:10:16 -0500

    simple (dumb) rpc server for geokit api

--- gips/datahandler/geokit_api.py
+++ gips/datahandler/geokit_api.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+import datetime
+
 from django.forms.models import model_to_dict
 from gips.inventory import orm
 orm.setup()
@@ -8,6 +10,7 @@ from gips.datahandler import api
 
 
 def get_datacatalog ():
+    # TODO: return list of dicts
     pass
 
 
@@ -35,7 +38,10 @@ def stats_request_results(request_id):
     qs = Result.objects.filter(job=request_id['job'])
     result_list = []
     for result in qs:
-        result_list.append(model_to_dict(result))
+        r = model_to_dict(result)
+        # xmlrpclib doesn't like datetime.date, convert to datetime.datetime
+        r['date'] = datetime.datetime.fromordinal(r['date'].toordinal())
+        result_list.append(r)
 
     return result_list
 
@@ -55,7 +61,10 @@ def stats_request_results_filter(request_id, filters):
     qs.filter(**filters)
     result_list = []
     for result in qs:
-        result_list.append(model_to_dict(result))
+        r = model_to_dict(result)
+        # xmlrpclib doesn't like datetime.date, convert to datetime.datetime
+        r['date'] = datetime.datetime.fromordinal(r['date'].toordinal())
+        result_list.append(r)
 
     return result_list
     #return Result.objects.filter(feature_set).filter(filters)
