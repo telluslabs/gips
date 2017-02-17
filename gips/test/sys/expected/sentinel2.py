969ff3d6dbdc42655cbe99c218c791e0badf8f66
Tom Olson <tolson@appliedgeosolutions.com>
2017-02-17 14:00:22 -0500

    200 sentinel2 non-fetch inventory sys test

---
+++ gips/test/sys/expected/sentinel2.py
@@ -0,0 +1,23 @@
+"""Known-good outcomes for tests, mostly stdout and created files."""
+
+t_inventory = { 'stdout': u"""\x1b[1mGIPS Data Inventory (v0.8.2)\x1b[0m
+Retrieving inventory for site durham-0
+
+\x1b[1mAsset Coverage for site durham-0\x1b[0m
+\x1b[1m
+Tile Coverage
+\x1b[4m  Tile      % Coverage   % Tile Used\x1b[0m
+   19TCH      100.0%        0.1%
+
+\x1b[1m\x1b[4m    DATE       L1C     Product  \x1b[0m
+\x1b[1m2017        
+\x1b[0m    003       100.0%   
+    010       100.0%   
+
+
+2 files on 2 dates
+\x1b[1m
+SENSORS\x1b[0m
+\x1b[35mMSI: Multispectral Instrument\x1b[0m
+"""
+}
---
+++ gips/test/sys/t_sentinel2.py
@@ -0,0 +1,39 @@
+import logging
+from datetime import datetime
+
+import pytest
+import envoy
+
+from .util import *
+
+logger = logging.getLogger(__name__)
+
+pytestmark = sys # skip everything unless --sys
+
+driver = 'sentinel2'
+
+# changing this will require changes in expected/
+# should be two assets, one for jan 3, one for jan 10 ------vvv------vvv also both are tile 19TCH
+STD_ARGS = ('sentinel2', '-s', DURHAM_SHP_PATH, '-d', '2017-003,2017-010', '-v', '4')
+
+@pytest.fixture
+def setup_data(pytestconfig):
+    """Use gips_inventory to ensure presence of data in the data repo."""
+    if not pytestconfig.getoption('setup_repo'):
+        logger.debug("Skipping repo setup per lack of option.")
+        return
+    logger.info("Downloading Sentinel 2 data . . .")
+    cmd_str = 'gips_inventory ' + ' '.join(STD_ARGS) + ' --fetch'
+    outcome = envoy.run(cmd_str)
+    logger.info("Data download complete.")
+    if outcome.status_code != 0:
+        raise RuntimeError("Data setup via `gips_inventory` failed",
+                           outcome.std_out, outcome.std_err, outcome)
+
+
+def t_inventory(setup_data, repo_env, expected):
+    """Test `gips_inventory` and confirm recorded output is given."""
+    actual = repo_env.run('gips_inventory', *STD_ARGS)
+    assert expected == actual
+
+# TODO for tests here down, use modis as a guide (eg t_process etc etc)
