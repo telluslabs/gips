ab2e00f5dab4e9781b92221d129a3c8a1873986f
bhbraswell <rbraswell@appliedgeosolutions.com>
2017-03-28 13:30:21 -0400

    starting on 299

--- install_nile.sh
+++ install_nile.sh
@@ -12,24 +12,24 @@ echo === install system deps ===
 # sudo apt-get install python-software-properties
 
 # this is needed
-sudo apt-get update
+#sudo apt-get update
 
 # sudo add-apt-repository ppa:ubuntugis/ppa # ubuntugis/unstable moved to GDAL2.0
 
 # TODO I doubt *ALL* the boost libs are needed and there are MANY of them;
 # would be great to reduce the bulk
-sudo apt-get install python g++ gfortran swig \
-                     libboost-all-dev libfreetype6-dev libgnutls-dev \
-                     libatlas-base-dev libgdal-dev libgdal1-dev gdal-bin \
-                     python-pip python-numpy python-scipy python-gdal
+#sudo apt-get install python g++ gfortran swig \
+#                     libboost-all-dev libfreetype6-dev libgnutls-dev \
+#                     libatlas-base-dev libgdal-dev libgdal1-dev gdal-bin \
+#                     python-pip python-numpy python-scipy python-gdal
 
 # On some non-Ubuntu systems, python-virtualenv takes the place of virtualenv.
 # Install the one that works.
-sudo apt-get install virtualenv || sudo apt-get install python-virtualenv
+#sudo apt-get install virtualenv || sudo apt-get install python-virtualenv
 
 echo === clone source repo and setup virtualenv ===
-virtualenv .venv --system-site-packages
-source .venv/bin/activate
+virtualenv .venv_nile --system-site-packages
+source .venv_nile/bin/activate
 
 echo === install a few dependencies via pip ===
 pip install -r dev_requirements.txt
