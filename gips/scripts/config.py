2b4dd3096758e465fe5b2dd8f65c74efdcaf6cfe
ircwaves <ircwaves@gmail.com>
2017-05-04 11:35:06 -0400

    updated to pull in driver config snippets

--- gips/scripts/config.py
+++ gips/scripts/config.py
@@ -25,8 +25,6 @@ import os, sys
 import pprint
 import traceback
 
-import django
-from django.core.management import call_command
 
 import gips
 from gips import __version__ as version
@@ -37,9 +35,13 @@ from gips.inventory import orm
 
 
 def migrate_database():
-    """Migrate the database if the ORM is turned on."""
+    """
+    TODO: move this into orm
+    Migrate the database if the ORM is turned on.
+    """
     if not orm.use_orm():
         return
+    from django.core.management import call_command
     print 'Migrating database'
     orm.setup()
     call_command('migrate', interactive=False)
@@ -70,11 +72,49 @@ def main():
     parser = GIPSParser(description=title, datasources=False)
     subparser = parser.add_subparsers(dest='command')
     subparser.add_parser('print', help='Print current settings')
-    p = subparser.add_parser('env', help='Configure GIPS repositories in this environment')
-    p.add_argument('-r', '--repos', help='Top level directory for repositories', default='/data/repos')
-    p.add_argument('-e', '--email', help='Set email address (used for anonymous FTP sources)', default='')
+    p = subparser.add_parser(
+        'env',
+        help='Configure GIPS repositories in this environment'
+    )
+    p.add_argument(
+        '-r', '--repos', help='Top level directory for repositories',
+        default='/data/repos'
+    )
+    p.add_argument(
+        '-e', '--email',
+        help='Set email address (used for anonymous FTP sources)', default=''
+    )
+    p.add_argument(
+        '-d', '--drivers', nargs='*',
+        default=('modis', 'merra', 'landsat', 'aod'),
+        help='List of drivers to enable for this installation',
+    )
+    p.add_argument(
+        '-U', '--user',
+        help='Set username for EARTHDATA login', default=''
+    )
+    p.add_argument(
+        '-P', '--password',
+        help='Set password for EARTHDATA login', default=''
+    )
+    p.add_argument(
+        '-u', '--update',
+        help='Overwrite any existing settings file with these options',
+        default=False, action='store_true',
+    )
     p = subparser.add_parser('user', help='Configure GIPS repositories for this user (for per user customizations)')
-    #p.add_argument('-e', '--email', help='Set email address (used for anonymous FTP sources)')
+    p.add_argument(
+        '-U', '--user',
+        help='Set username for EARTHDATA login', default=''
+    )
+    p.add_argument(
+        '-P', '--password',
+        help='Set password for EARTHDATA login', default=''
+    )
+    p.add_argument(
+        '-e', '--email',
+        help='Set email address (used for anonymous FTP sources)'
+    )
     #h = 'Install full configuration file without inheriting from environment settings'
     #p.add_argument('-f', '--full', help=h, default=False, action='store_true')
     args = parser.parse_args()
@@ -97,7 +137,9 @@ def main():
 
     elif args.command == 'env':
         try:
-            cfgfile = create_environment_settings(args.repos, email=args.email)
+            cfgfile = create_environment_settings(
+                args.repos, email=args.email, drivers=args.drivers
+            )
             print 'Environment settings file: %s' % cfgfile
             print 'Creating repository directories'
             create_repos()
