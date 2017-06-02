71bc25cc7a0de8d84bb9ac5ae45396cd0d22feec
ircwaves <ircwaves@gmail.com>
2017-06-02 13:51:02 -0400

    fixed hangup

--- lxc.sh
+++ lxc.sh
@@ -13,9 +13,11 @@
 # +
 set -e
 CONT=dh
-{
+SNAP=pipped
+
+function create_or_reset_container() {
     lxc info $CONT 2>/dev/null 1>/dev/null || {
-        time lxc launch ubuntu-daily:16.04 $CONT
+        lxc launch ubuntu-daily:16.04 $CONT
         sleep 10
         lxc exec $CONT -- apt-get update
         sleep 10
@@ -23,20 +25,28 @@ CONT=dh
         # extra due to strange apt fetching issue
         lxc exec $CONT -- apt-get install -y gfortran libboost-all-dev libfreetype6-dev libgnutls-dev libatlas-base-dev libgdal-dev libgdal1-dev gdal-bin python-numpy python-scipy python-gdal swig2.0
         lxc exec $CONT -- pip install -U pip
-    }
-    echo "1"
-    lxc info $CONT | grep Snapshots > /dev/null && echo "2" &&
-       { echo "3" ; lxc restore $CONT pipped ; echo "4" ; } ||
-           { echo "5" ; lxc snapshot $CONT pipped ; echo "6" ; }
+    } ;
+
+    HAVE_SNAP=$(lxc info $CONT | grep ${SNAP} || true)
+    echo "HAVE_SNAP: $HAVE_SNAP"
+    if [ "$HAVE_SNAP" ] ; then
+        lxc restore $CONT $SNAP ;
+        echo "$CONT restored to snapshot $SNAP"
+    else
+        lxc snapshot $CONT $SNAP ;
+        echo "$CONT snapshot taken as $SNAP"
+    fi
     if $(lxc list $CONT | grep STOPPED) ; then
         lxc start $CONT
         sleep 1
     fi
 }
+
 read -sp "Enter your NASA EarthData username: " EDUSER
 echo ''
 read -sp "Enter your NASA EarthData password: " EDPASS
 echo ''
+create_or_reset_container
 lxc file push install_datahandler.py $CONT/root/
 lxc exec $CONT -- python /root/install_datahandler.py \
     --drivers modis merra \
