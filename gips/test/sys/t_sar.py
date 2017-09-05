2b1a34e7be4fca2b7fc960c435bf20294e05a7eb
ircwaves <ircwaves@gmail.com>
2017-09-05 11:33:52 -0400

    tests results verified

--- gips/test/sys/t_sar.py
+++ gips/test/sys/t_sar.py
@@ -13,9 +13,9 @@ pytestmark = sys  # skip everything unless --sys
 
 # changing this will require changes in expected/
 driver = 'sar'
-STD_TILE = 'N07E099'
-STD_DATES = '2015-04-11'
-STD_ARGS = (driver, '-t', STD_TILE, '-d', STD_DATES, '-v', '4')
+STD_TILE = 'N07E099 N19E100 N00E099'
+STD_DATES = '2009,2015'
+TILED_ARGS = (driver, '-t', 'N07E099', 'N19E100', 'N00E099', '-d', STD_DATES, '-v', '4')
 ASSET_URL_PREFIX = (
     'ftp://anonymous:{USER}@volga.ags.io/files/{DRIVER}/'
     .format(
@@ -66,7 +66,7 @@ def setup_sar_data(pytestconfig):
                 outcome.std_out, outcome.std_err, outcome
             )
     else:
-        cmd_str = 'gips_inventory ' + ' '.join(STD_ARGS) + ' --fetch'
+        cmd_str = 'gips_inventory ' + ' '.join(TILED_ARGS) + ' --fetch'
         logger.info("Downloading SAR assets with " + cmd_str)
         outcome = envoy.run(cmd_str)
         logger.info("SAR data download complete.")
@@ -86,14 +86,14 @@ def t_inventory(setup_fixture, repo_env, expected):
     """
     Test `gips_inventory` for {} and confirm recorded output is given.
     """.format(driver)
-    actual = repo_env.run('gips_inventory', *STD_ARGS)
+    actual = repo_env.run('gips_inventory', *TILED_ARGS)
     assert expected == actual
 
 
 def t_process(setup_fixture, repo_env, expected):
     """Test gips_process on {} data.""".format(driver)
-    process_actual = repo_env.run('gips_process', *STD_ARGS)
-    inventory_actual = envoy.run('gips_inventory ' + ' '.join(STD_ARGS))
+    process_actual = repo_env.run('gips_process', *TILED_ARGS)
+    inventory_actual = envoy.run('gips_inventory ' + ' '.join(TILED_ARGS))
     assert expected == process_actual
     assert inventory_actual.std_out == expected._inv_stdout
 
@@ -103,48 +103,3 @@ def t_info(repo_env, expected):
     actual = repo_env.run('gips_info', driver)
     assert expected == actual
 
-
-def t_project(setup_fixture, clean_repo_env, output_tfe, expected):
-    """Test gips_project {} with warping.""".format(driver)
-    args = STD_ARGS + ('--res', '100', '100', '--outdir', OUTPUT_DIR, '--notld')
-    actual = output_tfe.run('gips_project', *args)
-    assert expected == actual
-
-
-def t_project_no_warp(setup_fixture, clean_repo_env, output_tfe, expected):
-    """Test gips_project {} without warping.""".format(driver)
-    args = STD_ARGS + ('--outdir', OUTPUT_DIR, '--notld')
-    actual = output_tfe.run('gips_project', *args)
-    assert expected == actual
-
-
-# Haven't used gips_tiles ever
-def t_tiles(setup_fixture, clean_repo_env, output_tfe, expected):
-    """Test gips_tiles {} with warping.""".format(driver)
-    args = STD_ARGS + ('--outdir', OUTPUT_DIR, '--notld')
-    actual = output_tfe.run('gips_tiles', *args)
-    assert expected == actual
-
-
-def t_tiles_copy(setup_fixture, clean_repo_env, output_tfe, expected):
-    """Test gips_tiles {} with copying.""".format(driver)
-    # doesn't quite use STD_ARGS
-    args = (driver, '-t', STD_TILE, '-d', STD_DATES, '-v', '4',
-            '--outdir', OUTPUT_DIR, '--notld')
-    actual = output_tfe.run('gips_tiles', *args)
-    assert expected == actual
-
-
-def t_stats(setup_fixture, clean_repo_env, output_tfe, expected):
-    """Test gips_stats on projected files."""
-    # generate data needed for stats computation
-    args = STD_ARGS + ('--res', '100', '100', '--outdir', OUTPUT_DIR, '--notld')
-    prep_run = output_tfe.run('gips_project', *args)
-    assert prep_run.exit_status == 0  # confirm it worked; not really in the test
-
-    # compute stats
-    gtfe = GipsTestFileEnv(OUTPUT_DIR, start_clear=False)
-    actual = gtfe.run('gips_stats', OUTPUT_DIR)
-
-    # check for correct stats content
-    assert expected == actual
