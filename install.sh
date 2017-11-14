ab3756627da61cdffdc3c6760d38bca90825cb8e
bhbraswell <rbraswell@appliedgeosolutions.com>
2017-11-14 11:46:53 -0500

    new install, simple, works for me

--- install.sh
+++ install.sh
@@ -1,47 +1,33 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 set -e
-
-# this script needs to run in the clone dir
 git remote show origin | grep 'Fetch URL: .*gips.git$' -q
-test -d .git
 
-echo === install system deps ===
-# TODO are these needed?
-# innstall UbuntuGIS repository
-# sudo apt-get install python-software-properties
+echo -n "Enter full path to data repository:"
+read ARCHIVEDIR
 
-# this is needed
+echo -n "Enter your email address"
+read EMAIL
+
 sudo apt-get update
 
-# sudo add-apt-repository ppa:ubuntugis/ppa # ubuntugis/unstable moved to GDAL2.0
+sudo apt-get install git virtualenv python python-pip gfortran libboost-all-dev\
+     libfreetype6-dev libgnutls-dev libatlas-base-dev libgdal-dev libgdal1-dev\
+     gdal-bin python-numpy python-scipy python-gdal swig2.0
 
-# TODO I doubt *ALL* the boost libs are needed and there are MANY of them;
-# would be great to reduce the bulk
-sudo apt-get install python g++ gfortran swig \
-                     libboost-all-dev libfreetype6-dev libgnutls-dev \
-                     libatlas-base-dev libgdal-dev libgdal1-dev gdal-bin \
-                     python-pip python-numpy python-scipy python-gdal
+sudo -H pip install -U pip>=9.0.1
 
-# On some non-Ubuntu systems, python-virtualenv takes the place of virtualenv.
-# Install the one that works.
-sudo apt-get install virtualenv || sudo apt-get install python-virtualenv
+virtualenv --system-site-packages venv
 
-echo === clone source repo and setup virtualenv ===
-virtualenv .venv --system-site-packages
-source .venv/bin/activate
+source venv/bin/activate
 
-echo === install a few dependencies via pip ===
-pip install -r dev_requirements.txt
-# gippy has to be done this way because gippy stopped being tracked in pypi
-pip install 'https://github.com/Applied-GeoSolutions/gippy/tarball/v0.3.x#egg=gippy-0.3.8-'`date +%Y%m%d`
+pip install -r requirements.txt
 
-echo === install GIPS itself ===
-# TODO --process-dependency-links is deprecated
-pip install -e . --process-dependency-links
+pip install --process-dependency-links -e .
 
-# help user with configuration
-echo "Install complete.  GIPS configuration:"
-if ! gips_config print; then
-    echo 'Configure GIPS with:  `gips_config env -e <emailaddr> -r <full-path-to-desired-repo>`.'
-fi
+gips_config env -r $ARCHIVEDIR -e $EMAIL
+gips_config user
+
+echo ""
+echo "Now edit settings.py for things like EARTHDATA password"
+echo "gips_project modis --fetch -s gips/test/NHseacoast.shp -d 2017-5-25 -v5 -p ndvi8"
