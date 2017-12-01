aa26dc5d408b43c5393b6be9e643f104ac385cfd
Rick Emery <remery@appliedgeosolutions.com>
2017-12-01 16:10:47 +0000

    changing to postgis db

--- gips/datahandler/settings_template.py
+++ gips/datahandler/settings_template.py
@@ -8,8 +8,8 @@ LOG_LEVEL     = 1
 EXPORT_DIR    = '$DH_EXPORT_DIR'
 
 DATABASES['inventory'] = {
-    #'ENGINE': 'django.contrib.gis.db.backends.postgis',
-    'ENGINE': 'django.db.backends.postgresql',
+    'ENGINE': 'django.contrib.gis.db.backends.postgis',
+    #'ENGINE': 'django.db.backends.postgresql',
     'NAME': '$DB_NAME',
     'USER': '$DB_USER',
     'PASSWORD': '$DB_PASSWORD',
