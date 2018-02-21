2c91589a8d6f4cf96129335400e5122dcc157b52
Tom Olson <tolson@appliedgeosolutions.com>
2018-02-21 12:18:43 -0500

    420 improve unit test coverage:
    This rounds out coverage of landsatAsset's query_* and fetch_* methods.
    Also a little refactoring crept in; couldn't restrain myself.

--- gips/test/unit/t_landsat.py
+++ gips/test/unit/t_landsat.py
@@ -30,34 +30,67 @@ def t_landsatAsset_constructor(fn, expected, mocker):
     la = landsat.landsatAsset(fn)
     assert expected == actual(la)
 
-def t_landsatAsset_query_service_not_available_case(mocker):
-    """Confirms the query_service method gives up if dates are invalid."""
-    mocker.patch.object(landsat.landsatAsset, 'available').return_value = False
+@pytest.mark.parametrize(
+        'avail_rv, a_type, get_setting_rv, query_service_rv',
+        [(False, 'dontcare', 'dontcare', None),
+         (True,  'DN',       'dontcare', None),
+         (True,  'C1',       'invalid-data-source', ValueError)])
+def t_landsatAsset_query_service_early_exits(
+            mocker, avail_rv, a_type, get_setting_rv, query_service_rv):
+    """Confirms the query_service method gives up early when it should."""
+    mocker.patch.object(landsat.landsatAsset, 'available',
+                        return_value=avail_rv)
+    mocker.patch.object(landsat.landsatAsset.Repository, 'get_setting',
+                        return_value=get_setting_rv)
     dc = 'dontcare'
-    # if it doesn't exit in time, this fake date will raise an exception when
-    # query_service tries to call strftime on it -------vv
-    assert None == landsat.landsatAsset.query_service(dc, dc, dc)
+    if query_service_rv is ValueError:
+        with pytest.raises(ValueError):
+            landsat.landsatAsset.query_service(a_type, dc, dc)
+    else:
+        assert None == landsat.landsatAsset.query_service(a_type, dc, dc)
 
-def t_landsatAsset_query_service_success_case(mocker):
+@pytest.fixture
+def m_load_ee_search_keys(mocker):
+    return mocker.patch.object(landsat.landsatAsset, 'load_ee_search_keys')
+
+@pytest.fixture
+def m_get_setting(mocker):
+    return mocker.patch.object(landsat.landsatAsset.Repository, 'get_setting')
+
+@pytest.fixture
+def m_ee_login(mocker):
+    return mocker.patch.object(landsat.landsatAsset, 'ee_login')
+
+@pytest.yield_fixture
+def m_usgs_lib(mocker):
+    """landsat does 'import usgs' in local scope; work around that"""
+    # TODO try mocker.patch.dict
+    saved_usgs = sys.modules.get('usgs', None)
+    m_usgs = sys.modules['usgs'] = mocker.MagicMock()
+    yield m_usgs
+    if saved_usgs is not None:
+        sys.modules['usgs'] = saved_usgs
+
+@pytest.fixture
+def m_utils_make_temp_dir(mocker):
+    m_context_manager = mocker.patch.object(landsat.utils, 'make_temp_dir'
+                                            ).return_value
+    # in 'with blah.blah() as x:', x == 'fake-temp-dir'
+    m_context_manager.__enter__.return_value = 'fake-temp-dir'
+    return m_context_manager
+
+def t_landsatAsset_query_service_c1_success_case(
+        mocker, m_ee_login, m_get_setting, m_load_ee_search_keys, m_usgs_lib):
     """Confirms method works for the normal case."""
     ### big pile o' mocks
-    mocker.patch.object(landsat.landsatAsset.Repository, 'get_setting',
-                        return_value='usgs')
-    mocker.patch.object(landsat.landsatAsset, 'load_ee_search_keys')
-    mocker.patch.object(landsat.landsatAsset, 'ee_login')
-
-    # have to mock around a local import
-    m_usgs, m_api = mocker.MagicMock(), mocker.MagicMock()
-    sys.modules['usgs'] = m_usgs
-    m_usgs.api = m_api
-
+    m_get_setting.return_value = 'usgs'
     m_dataset = mocker.MagicMock()
     mocker.patch.object(landsat.landsatAsset, '_ee_datasets'
                         ).keys.return_value = [m_dataset]
     result = {'metadataUrl': 'dontcare', 'displayId': 'basename',
               'entityId': 'scene-id'}
     # response object
-    m_api.search.return_value = {'data': {'results': [result]}}
+    m_usgs_lib.api.search.return_value = {'data': {'results': [result]}}
     mocker.patch.object(landsat.requests, 'get')
     # why do people use XML?  Trick question:  XML uses you, and not gently.
     m_xml = mocker.patch.object(landsat.ElementTree, 'fromstring').return_value
@@ -75,3 +108,102 @@ def t_landsatAsset_query_service_success_case(mocker):
                                                 datetime.date(2016, 1, 1))
 
     assert expected == actual
+
+sample_c1s3_keys = {
+    '_15m_tif':
+        'c1/L8/027/033/LC08_L1TP_027033_20170506_20170515_01_T1/LC08_L1TP_027033_20170506_20170515_01_T1_B8.TIF',
+    '_30m_tifs': [
+        'c1/L8/027/033/LC08_L1TP_027033_20170506_20170515_01_T1/LC08_L1TP_027033_20170506_20170515_01_T1_B1.TIF',
+        'c1/L8/027/033/LC08_L1TP_027033_20170506_20170515_01_T1/LC08_L1TP_027033_20170506_20170515_01_T1_B2.TIF',
+        'c1/L8/027/033/LC08_L1TP_027033_20170506_20170515_01_T1/LC08_L1TP_027033_20170506_20170515_01_T1_B3.TIF',
+        'c1/L8/027/033/LC08_L1TP_027033_20170506_20170515_01_T1/LC08_L1TP_027033_20170506_20170515_01_T1_B4.TIF',
+        'c1/L8/027/033/LC08_L1TP_027033_20170506_20170515_01_T1/LC08_L1TP_027033_20170506_20170515_01_T1_B5.TIF',
+        'c1/L8/027/033/LC08_L1TP_027033_20170506_20170515_01_T1/LC08_L1TP_027033_20170506_20170515_01_T1_B6.TIF',
+        'c1/L8/027/033/LC08_L1TP_027033_20170506_20170515_01_T1/LC08_L1TP_027033_20170506_20170515_01_T1_B7.TIF',
+        'c1/L8/027/033/LC08_L1TP_027033_20170506_20170515_01_T1/LC08_L1TP_027033_20170506_20170515_01_T1_B9.TIF',
+        'c1/L8/027/033/LC08_L1TP_027033_20170506_20170515_01_T1/LC08_L1TP_027033_20170506_20170515_01_T1_B10.TIF',
+        'c1/L8/027/033/LC08_L1TP_027033_20170506_20170515_01_T1/LC08_L1TP_027033_20170506_20170515_01_T1_B11.TIF',
+    ],
+    'qa_tif':
+        'c1/L8/027/033/LC08_L1TP_027033_20170506_20170515_01_T1/LC08_L1TP_027033_20170506_20170515_01_T1_BQA.TIF',
+    'mtl_txt':
+        'c1/L8/027/033/LC08_L1TP_027033_20170506_20170515_01_T1/LC08_L1TP_027033_20170506_20170515_01_T1_MTL.txt',
+}
+
+@pytest.yield_fixture
+def c1s3_cache_control():
+    saved_cache = landsat.landsatAsset._query_s3_cache
+    landsat.landsatAsset._query_s3_cache = (None, None)
+    yield
+    landsat.landsatAsset._query_s3_cache = saved_cache
+
+def t_landsatAsset_query_service_c1s3_success_case(mocker, c1s3_cache_control):
+    """Confirms method works for the normal case."""
+    ### big pile o' mocks
+    mocker.patch.object(landsat.landsatAsset.Repository, 'get_setting',
+                        return_value='s3')
+    mocker.patch.dict(landsat.os.environ, {
+            'AWS_SECRET_ACCESS_KEY': 'fake-secret-key',
+            'AWS_ACCESS_KEY_ID': 'fake-key-id'})
+
+    # have to mock around a local import
+    m_boto3 = sys.modules['boto3'] = mocker.MagicMock()
+    m_s3 = m_boto3.resource.return_value
+    flattened_keys = sample_c1s3_keys['_30m_tifs'] + [
+        sample_c1s3_keys[k] for k in ('qa_tif', '_15m_tif', 'mtl_txt')]
+    filter_output = []
+    for key in flattened_keys:
+        mm = mocker.MagicMock()
+        mm.key = key
+        filter_output.append(mm)
+    m_s3.Bucket.return_value.objects.filter.return_value = filter_output
+
+    expected = sample_c1s3_keys.copy()
+    expected['basename'] = 'LC08_L1TP_027033_20170506_20170515_01_T1_S3.json'
+    actual = landsat.landsatAsset.query_service('C1S3', '027033',
+                                                datetime.date(2017, 5, 6))
+    assert expected == actual
+
+def t_landsatAsset_fetch_c1(
+        mocker, m_ee_login, m_get_setting, m_usgs_lib, m_utils_make_temp_dir):
+    #m_get_setting.return_value = 'dontcare' # just
+    m_get_setting.return_value = 'driver-dir'
+    mocker.patch.object(landsat.homura, 'download')
+    mocker.patch.object(landsat.os, 'listdir'
+                        ).return_value = ['fake-asset.tar.gz']
+    m_rename = mocker.patch.object(landsat.os, 'rename')
+    dc = 'dontcare'
+    landsat.landsatAsset.fetch('C1', dc, dc, dc, scene_id=dc, dataset=dc)
+    expected = mocker.call('fake-temp-dir/fake-asset.tar.gz',
+                'driver-dir/stage/fake-asset.tar.gz')
+    assert (m_rename.call_count == 1
+            and expected == m_rename.call_args)
+
+sample_c1s3_asset_content = {
+    u'15m-band': u'/vsis3_streaming/landsat-pds/c1/L8/027/033/LC08_L1TP_027033_20170506_20170515_01_T1/LC08_L1TP_027033_20170506_20170515_01_T1_B8.TIF',
+    u'30m-bands': [
+        u'/vsis3_streaming/landsat-pds/c1/L8/027/033/LC08_L1TP_027033_20170506_20170515_01_T1/LC08_L1TP_027033_20170506_20170515_01_T1_B1.TIF',
+        u'/vsis3_streaming/landsat-pds/c1/L8/027/033/LC08_L1TP_027033_20170506_20170515_01_T1/LC08_L1TP_027033_20170506_20170515_01_T1_B2.TIF',
+        u'/vsis3_streaming/landsat-pds/c1/L8/027/033/LC08_L1TP_027033_20170506_20170515_01_T1/LC08_L1TP_027033_20170506_20170515_01_T1_B3.TIF',
+        u'/vsis3_streaming/landsat-pds/c1/L8/027/033/LC08_L1TP_027033_20170506_20170515_01_T1/LC08_L1TP_027033_20170506_20170515_01_T1_B4.TIF',
+        u'/vsis3_streaming/landsat-pds/c1/L8/027/033/LC08_L1TP_027033_20170506_20170515_01_T1/LC08_L1TP_027033_20170506_20170515_01_T1_B5.TIF',
+        u'/vsis3_streaming/landsat-pds/c1/L8/027/033/LC08_L1TP_027033_20170506_20170515_01_T1/LC08_L1TP_027033_20170506_20170515_01_T1_B6.TIF',
+        u'/vsis3_streaming/landsat-pds/c1/L8/027/033/LC08_L1TP_027033_20170506_20170515_01_T1/LC08_L1TP_027033_20170506_20170515_01_T1_B7.TIF',
+        u'/vsis3_streaming/landsat-pds/c1/L8/027/033/LC08_L1TP_027033_20170506_20170515_01_T1/LC08_L1TP_027033_20170506_20170515_01_T1_B9.TIF',
+        u'/vsis3_streaming/landsat-pds/c1/L8/027/033/LC08_L1TP_027033_20170506_20170515_01_T1/LC08_L1TP_027033_20170506_20170515_01_T1_B10.TIF',
+        u'/vsis3_streaming/landsat-pds/c1/L8/027/033/LC08_L1TP_027033_20170506_20170515_01_T1/LC08_L1TP_027033_20170506_20170515_01_T1_B11.TIF'],
+    u'mtl': u'https://landsat-pds.s3.amazonaws.com/c1/L8/027/033/LC08_L1TP_027033_20170506_20170515_01_T1/LC08_L1TP_027033_20170506_20170515_01_T1_MTL.txt',
+    u'qa-band': u'/vsis3_streaming/landsat-pds/c1/L8/027/033/LC08_L1TP_027033_20170506_20170515_01_T1/LC08_L1TP_027033_20170506_20170515_01_T1_BQA.TIF',
+}
+
+def t_landsatAsset_fetch_s3(mocker, m_get_setting, m_utils_make_temp_dir):
+    """Check the content passed in to json.dump to confirm the method."""
+    mocker.patch.object(landsat, 'open')
+    m_json_dump = mocker.patch.object(landsat, 'json').dump
+    m_shutil_copy = mocker.patch.object(landsat.shutil, 'copy')
+
+    dc = 'dontcare'
+    landsat.landsatAsset.fetch('C1S3', dc, dc, dc, **sample_c1s3_keys)
+
+    # this means "the first argument of the call" -----------vvvvvv
+    assert sample_c1s3_asset_content == m_json_dump.call_args[0][0]
