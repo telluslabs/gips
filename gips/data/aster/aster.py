9f1c0901546f3e8d58a0e116f0e672ed9bc666a4
Rick Emery <remery@ags.io>
2018-02-23 16:39:56 +0000

    Make ref

--- gips/data/aster/aster.py
+++ gips/data/aster/aster.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 ################################################################################
 #    GIPS: Geospatial Image Processing System
 #
@@ -24,29 +25,40 @@
 from __future__ import print_function
 
 import os
-import sys
 import re
 import datetime
 import shapely
-import urllib
-import urllib2
-
-import math
 import numpy as np
-import requests
-
 import xml.etree.ElementTree as ET
 
-import gippy
-from gippy.algorithms import Indices
 from gips.data.core import Repository, Asset, Data
-from gips.utils import VerboseOut, settings, open_vector
+from gips.utils import VerboseOut, settings, open_vector, basename
 from gips.inventory import dbinv
 from gips import utils
+from osgeo import gdal, osr
 
 from shapely.wkt import loads
 
 from pyCMR.pyCMR import CMR
+
+
+# Order for ucc (Abrams, 1999) is: Band 1 high, normal, low, low2;
+# Band 2 h, n, l1, l2; b3 h, n, l1, l2 (3N & 3B the same)
+# Construct a dataframe for the UCC values:
+ucc = np.matrix(([
+                [0.676, 1.688, 2.25, 0.0],
+                [0.708, 1.415, 1.89, 0.0],
+                [0.423, 0.862, 1.15, 0.0],
+                [0.1087, 0.2174, 0.2900, 0.2900],
+                [0.0348, 0.0696, 0.0925, 0.4090],
+                [0.0313, 0.0625, 0.0830, 0.3900],
+                [0.0299, 0.0597, 0.0795, 0.3320],
+                [0.0209, 0.0417, 0.0556, 0.2450],
+                [0.0159, 0.0318, 0.0424, 0.2650]]))
+
+# Thome et al. is used, which uses spectral irradiance values from MODTRAN
+# Ordered b1, b2, b3N, b4, b5...b9
+irradiance = [1848, 1549, 1114, 225.4, 86.63, 81.85, 74.85, 66.49, 59.85]
 
 
 def binmask(arr, bit):
@@ -58,7 +70,7 @@ def binmask(arr, bit):
 
 def get_temporal_string(date):
     start_date_str = date.strftime("%Y-%m-%dT00:00:00Z")
-    end_date_str =  date.strftime("%Y-%m-%dT23:59:58Z")
+    end_date_str = date.strftime("%Y-%m-%dT23:59:58Z")
     temporal_str = "%s,%s" % (start_date_str, end_date_str)
     return temporal_str
 
@@ -66,11 +78,7 @@ def get_temporal_string(date):
 def reproject_spatial_extent_wkt(spatent, target_epsg):
     from osgeo import ogr, osr
     vector = open_vector(spatent.feature[0])
-    shp = ogr.Open(vector.Filename())
-    if vector.LayerName() == '':
-        layer = shp.GetLayer(0)
-    else:
-        layer = shp.GetLayer(vector.LayerName())
+
     # create and warp site geometry
     ogrgeom = ogr.CreateGeometryFromWkt(spatent.wkt)
     src = osr.SpatialReference(vector.Projection())
@@ -84,22 +92,14 @@ def reproject_spatial_extent_wkt(spatent, target_epsg):
 
 
 def get_spatial_extent_projection(spatent):
-    from osgeo import ogr, osr
     vector = open_vector(spatent.feature[0])
-    shp = ogr.Open(vector.Filename())
-    if vector.LayerName() == '':
-        layer = shp.GetLayer(0)
-    else:
-        layer = shp.GetLayer(vector.LayerName())
-    # create and warp site geometry
-    src = osr.SpatialReference(vector.Projection())
-    return src
+    return osr.SpatialReference(vector.Projection())
 
 
 class asterRepository(Repository):
     name = 'Aster'
     description = ('Advanced Spaceborne Thermal Emission and Reflection '
-                    'Radiometer (ASTER)')
+                   'Radiometer (ASTER)')
 
     # NASA assets require special authentication
     _manager_url = "https://urs.earthdata.nasa.gov"
@@ -113,7 +113,6 @@ class asterRepository(Repository):
     def vector2tiles(cls, vector, pcov=0.0, ptile=0.0, tilelist=None):
         """ Return matching tiles and coverage % for provided vector """
         from osgeo import ogr, osr
-        #return {}
         # open tiles vector
         v = open_vector(cls.get_setting('tiles'))
         shp = ogr.Open(v.Filename())
@@ -163,6 +162,7 @@ class asterAsset(Asset):
     Repository = asterRepository
 
     _sensors = {
+        'L1T': {"description": "L1T Sensor"},
     }
 
     _assets = {
@@ -190,7 +190,6 @@ class asterAsset(Asset):
         self.date = datetime.datetime.strptime(year + month + dom, "%Y%m%d")
         self.tile = os.path.splitext(bname)[0]
 
-
     @classmethod
     def get_url(cls, asset, date, tilename):
         year = date.year
@@ -200,7 +199,6 @@ class asterAsset(Asset):
                 cls._assets[asset]['url'], year, month, day, tilename
         )
         return url
-
 
     @classmethod
     def query_service(cls, asset, feature, date):
@@ -255,7 +253,7 @@ class asterAsset(Asset):
                 return []
         available = []
 
-        for item in results: # Should be 1
+        for item in results:  # Should be 1
             # Extract URL and file name from search results
             basename = item['Granule']['DataGranule']['ProducerGranuleId']
             url = ""
@@ -269,10 +267,7 @@ class asterAsset(Asset):
             err_msg = 'Unable to find remote match at {}'.format(mainurl)
             utils.verbose_out(err_msg, 4)
 
-        #TODO: Retrieve geometries
-
         return available
-
 
     def get_geometry_name(self):
         return os.path.splitext(os.path.basename(self.filename))[0]
@@ -307,7 +302,6 @@ class asterAsset(Asset):
         wkt += "%s %s))" % (point[0].text, point[1].text)
         return wkt
 
-
     @classmethod
     def fetch(cls, asset, tile, date):
         retrieved_filenames = []
@@ -323,7 +317,7 @@ class asterAsset(Asset):
                 "Asset fetch error ({})".format(asset), continuable=True):
             response = cls.Repository.managed_request(url)
             if response is None:
-                return retrieved_filenames # give up now as the rest
+                return retrieved_filenames  # give up now as the rest
             with open(outpath, 'wb') as fd:
                 fd.write(response.read())
 
@@ -344,9 +338,10 @@ class asterData(Data):
         'ref': {
             'description': 'TOA reflectance',
             'assets': ['L1T'],
-            'bands': [{'name': 'temp', 'units': 'none' }],
+            'sensor': 'L1T',
+            'bands': [{'name': 'temp', 'units': 'none'}],
             'category': 'Temporary Stuff',
-            'startdate': datetime.date(2000,3,4),
+            'startdate': datetime.date(2000, 3, 4),
             'latency': 1,
         }
     }
@@ -379,9 +374,39 @@ class asterData(Data):
                 availassets.append(asset)
                 allsds.extend(sds)
                 #version = int(re.findall('M.*\.(\d{3})\.\d{13}\.hdf', sds[0])[0])
-                version = 1 # What is this?
+                version = 1  # What is this?
 
         return asset, version, missingassets, availassets, allsds
+
+    def ParseAndAddFiles(self, filenames=None):
+        """Parse and Add filenames to existing filenames.
+
+        If no filenames are provided, a list from find_files() is used
+        instead."""
+        if filenames is None:
+            filenames = self.find_files()  # find *product* files actually
+        datedir = self.Repository._datedir
+        for f in filenames:
+            bname = basename(f)
+            parts = bname.split('_')
+            if len(parts) < 3 or len(parts) > 8:
+                # Skip this file
+                VerboseOut('Unrecognizable file: %s' % f, 3)
+                continue
+            with utils.error_handler('Unrecognizable file ' + f, continuable=True):
+                # only admit product files matching a single date
+                if self.date is None:
+                    # First time through
+                    print("Parts[5]", parts[5])
+                    self.date = datetime.datetime.strptime(parts[5], datedir).date()
+                    print(self.date)
+                else:
+                    date = datetime.datetime.strptime(parts[5], datedir).date()
+                    if date != self.date:
+                        raise Exception('Mismatched dates: %s' % ' '.join(filenames))
+                sensor = parts[1]
+                product = parts[7]
+                self.AddFile(sensor, product, f, add_to_db=False)
 
     @Data.proc_temp_dir_manager
     def process(self, *args, **kwargs):
@@ -389,11 +414,168 @@ class asterData(Data):
         products = super(asterData, self).process(*args, **kwargs)
         if len(products) == 0:
             return
+        # These are the bands used by gippy
+        desired_sds_keys = [
+            'ImageData2',
+            'ImageData3N',
+        ]
+
+        # example products.requested:
+        # {'temp8tn': ['temp8tn'], 'clouds': ['clouds'], . . . }
+        # key is only used once far below, and val is only used for val[0].
+        for key, val in products.requested.items():
+            prod_type = val[0]
+            start = datetime.datetime.now()
+            asset, version, missingassets, availassets, allsds = \
+                self.asset_check(prod_type)
+
+            if not availassets:
+                # some products aren't available for every day but this is trying every day
+                VerboseOut('There are no available assets (%s) on %s for tile %s'
+                           % (str(missingassets), str(self.date), str(self.id), ), 5)
+                continue
+            meta = self.meta_dict()
+            meta['AVAILABLE_ASSETS'] = ' '.join(availassets)
+
+            sensor = self._products[prod_type]['sensor']
+            fname = self.temp_product_filename(sensor, prod_type)  # moved to archive at end of loop
+
+            # Get the sds' that gippy cares about
+            desired_sds = [s for s in allsds if s.split(':')[-1] in desired_sds_keys]
+
+            # Make ref
+            asset_fn = desired_sds[0].split('"')[1]
+            dn_image = gdal.Open(asset_fn)
+            aster_sds = dn_image.GetSubDatasets()
+            dn_meta = dn_image.GetMetadata()
+
+            # Correct for meta bug in GDAL 1
+            if (int(gdal.VersionInfo()) < 2000000):
+                meta_list = dn_image.GetMetadata_List()
+                dn_meta.update(dict([i.replace('=', '.').split(',') for i in meta_list if i.startswith('GAIN')]))
+
+            day = self.date.timetuple()
+            doy = day.tm_yday
+            # Calculate Earth-Sun Distance
+            esd = 1.0 - 0.01672 * np.cos(np.radians(0.9856 * (doy - 4)))
+
+            # Need SZA--calculate by grabbing solar elevation info
+            sza = [np.float(x) for x in dn_meta['SOLARDIRECTION'].split(', ')][1]
+            # Query gain data for each  band, needed for UCC
+            # Getting meta from first sds because of gdal v1 bug
+            gain_02 = dn_meta['GAIN.2'].strip()
+            gain_03n = dn_meta['GAIN.3'].strip()
+
+            # Define UL, LR, UTM zone
+            ul = [np.float(x) for x in dn_meta['UPPERLEFTM'].split(', ')]
+            lr = [np.float(x) for x in dn_meta['LOWERRIGHTM'].split(', ')]
+            utm = np.int(dn_meta['UTMZONENUMBER'])
+            n_s = np.float(dn_meta['NORTHBOUNDINGCOORDINATE'])
+
+            # Create UTM zone code numbers
+            utm_n = [i+32600 for i in range(60)]
+            utm_s = [i+32700 for i in range(60)]
+
+            # Define UTM zone based on North or South
+            # Unused?
+            if n_s < 0:
+                utm_zone = utm_s[utm]
+            else:
+                utm_zone = utm_n[utm]
+
+            del utm_n, utm_s, utm, dn_meta
+
+            # Loop through all ASTER L1T SDS (bands)
+            for i in range(len(aster_sds)):
+
+                # Maintain original dataset name
+                gname = str(aster_sds[i])
+                aster_sd = gname.split(',')[0]
+
+                vnir = re.search("(VNIR.*)", aster_sd)
+                swir = re.search("(SWIR.*)", aster_sd)
+                if vnir or swir:
+                    # Generate output name for tif
+                    aster_sd2 = aster_sd.split('(')[1]
+                    aster_sd3 = aster_sd2[1:-1]
+                    band = aster_sd3.split(':')[-1]
+
+                    # Open SDS and create array
+                    band_ds = gdal.Open(aster_sd3, gdal.GA_ReadOnly)
+                    sds = band_ds.ReadAsArray().astype(np.uint16)
+
+                    del aster_sd, aster_sd2, aster_sd3
+
+                    # Define extent and provide offset for UTM South zones
+                    if n_s < 0:
+                        ul_y = ul[0] + 10000000
+                        ul_x = ul[1]
+
+                        lr_y = lr[0] + 10000000
+                        lr_x = lr[1]
+
+                    # Define extent for UTM North zones
+                    else:
+                        ul_y = ul[0]
+                        ul_x = ul[1]
+
+                        lr_y = lr[0]
+                        lr_x = lr[1]
+
+                    # Query raster dimensions and calculate raster x and y resolution
+                    ncol, nrow = sds.shape
+                    y_res = -1 * round((max(ul_y, lr_y)-min(ul_y, lr_y))/ncol)
+                    x_res = round((max(ul_x, lr_x)-min(ul_x, lr_x))/nrow)
+
+                    # Define UL x and y coordinates based on spatial resolution
+                    ul_yy = ul_y - (y_res/2)
+                    ul_xx = ul_x - (x_res/2)
+
+                    if band == 'ImageData2':
+                        bn = -1 + 2
+                        # Query for gain specified in file metadata (by band)
+                        if gain_02 == 'HGH':
+                            ucc1 = ucc[bn, 0]
+                        elif gain_02 == 'NOR':
+                            ucc1 = ucc[bn, 1]
+                        else:
+                            ucc1 = ucc[bn, 2]
+
+                    if band == 'ImageData3N':
+                        bn = -1 + 3
+                        # Query for gain specified in file metadata (by band)
+                        if gain_03n == 'HGH':
+                            ucc1 = ucc[bn, 0]
+                        elif gain_03n == 'NOR':
+                            ucc1 = ucc[bn, 1]
+                        else:
+                            ucc1 = ucc[bn, 2]
+
+                    #Set irradiance value for specific band
+                    irradiance1 = irradiance[bn]
+
+                    # Convert from DN to Radiance
+                    rad = (sds-1.)*ucc1
+
+                    # Convert from Radiance to TOA Reflectance
+                    ref = (np.pi * rad * (esd * esd)) / (irradiance1 * np.sin(np.pi * sza / 180.))
+
+                    return
+                    # Make NDVI
+                    if prod_type == "ndvi":
+                        # Do stuff here
+                        pass
+
+            # add product to inventory
+            archive_fp = self.archive_temp_path(fname)
+            self.AddFile(sensor, key, archive_fp)
+            del dn_image  # to cover for GDAL's internal problems
+            utils.verbose_out(' -> {}: processed in {}'.format(
+                os.path.basename(fname), datetime.datetime.now() - start), level=1)
 
     @classmethod
     def findTiles(cls, asset, spatent, textent):
         return
-
 
     @classmethod
     def query_service(cls, products, spatent, textent, update=False, force=False, grouped=False):
@@ -409,7 +591,7 @@ class asterData(Data):
         if grouped:
             response = {}
         else:
-            response  = []
+            response = []
         tiles = {}
         for p in products:
             assets = cls.products2assets([p])
@@ -448,7 +630,7 @@ class asterData(Data):
                         local_assets = cls.Asset.discover(tile_name, d, a)
                         if force or len(local_assets) == 0 or update:
                             date_str = d.strftime("%F")
-                            prefix = (
+                            msg_prefix = (
                                 'query_service for {}, {}, {}'
                                 .format(a, tile_name, date_str)
                             )
@@ -461,10 +643,10 @@ class asterData(Data):
                                 #    raise Exception('should only be one asset')
                                 aobj = cls.Asset(tile_name)
 
-                                if (force or len(local_assets) == 0 or
-                                    (len(local_assets) == 1 and
-                                     local_assets[0].updated(aobj))
-                                ):
+                                if force or len(local_assets) == 0 or (
+                                     len(local_assets) == 1 and
+                                     local_assets[0].updated(aobj)
+                                    ):
                                     rec = {
                                         'product': p,
                                         'sensor': aobj.sensor,
@@ -488,10 +670,10 @@ class asterData(Data):
                                     # grouped by (p,t,d) there is a fair bit of
                                     # duplicated info this way, but oh well
                                     if grouped:
-                                        if (p,tile_name,d) in response:
-                                            response[(p,tile_name,d)].append(rec)
+                                        if (p, tile_name, d) in response:
+                                            response[(p, tile_name, d)].append(rec)
                                         else:
-                                            response[(p,tile_name,d)] = [rec]
+                                            response[(p, tile_name, d)] = [rec]
                                     else:
                                         response.append(rec)
             spatent.set_tiles(tiles)
@@ -518,4 +700,3 @@ class asterData(Data):
                     fetched.append((asset, tile, date))
 
         return fetched
-
