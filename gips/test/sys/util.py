15745d385d3fd3e47c94a58c3152cba91d51ca5a
Tom Olson <tolson@appliedgeosolutions.com>
2018-03-07 14:41:28 -0500

    518 avoid superfluous test failures by:
    truncating decimal places in gdalinfo-stats-based expectations.
    Updated t_process expectations to match.

--- gips/test/sys/util.py
+++ gips/test/sys/util.py
@@ -309,7 +309,7 @@ def generate_gdalinfo_stats_expectation(filename):
     sidecar = filename + '.aux.xml'
     os.path.exists(sidecar) and os.remove(sidecar) # RACE CONDITION LOL
     os.environ['GDAL_PAM_ENABLED'] = 'NO'
-    # TODO look at -json
+    # if needed switch to -json for more programmatic access to the data
     outcome = sh.gdalinfo('-stats', filename)
     # The output includes unpredictable file paths, so filter them out:
     lines = []
@@ -318,8 +318,13 @@ def generate_gdalinfo_stats_expectation(filename):
         # first detect whether we're in a 'Files:' block
         in_files_block = (l.startswith('Files: ')
                           or (in_files_block and l.startswith(' ')))
-        if not in_files_block:
-            lines.append(l)
+        if in_files_block:
+            continue
+
+        # stop at a certain decimal place for all decimal numbers
+        significant_digits = 8
+        sig_digits_re = r'(\.\d{' + str(significant_digits) + r'})\d+'
+        lines.append(re.sub(sig_digits_re, r'\1', l))
     return lines
 
 def params_from_expectations(expectations, mark_spec=None):
