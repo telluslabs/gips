ffb1891e17f964e78ac35e8b4131a1240f54a7a0
Bobby H Braswell <rbraswell@ags.io>
2018-03-19 11:11:44 -0400

    gips version env variable

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 rm -rf /Users/braswell/data/gips
 docker run --rm --name gips -h gips -v /Users/braswell/data/gips:/archive gips gips_config env
 tar xfvz composites.tgz -C /Users/braswell/data/gips/aod/
-docker run -it --rm --name gips -h gips -v /Users/braswell/data/gips:/archive gips
+# override version for testing only
+docker run -it --rm --name gips -h gips -e GIPS_OVERRIDE_VERSION=0.8.2 -v /Users/braswell/data/gips:/archive gips
