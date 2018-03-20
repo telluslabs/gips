c2a2a82ce1452b5d8d3f20c35aced6b49af2ca8e
Bobby H Braswell <rbraswell@ags.io>
2018-03-20 08:31:50 -0400

    need to organze test

--- run.sh
+++
@@ -1,5 +0,0 @@
-rm -rf /Users/braswell/data/gips
-docker run --rm --name gips -h gips -v /Users/braswell/data/gips:/archive gips gips_config env
-tar xfvz composites.tgz -C /Users/braswell/data/gips/aod/
-# override version for testing only
-docker run -it --rm --name gips -h gips -e GIPS_OVERRIDE_VERSION=0.8.2 -v /Users/braswell/data/gips:/archive gips
