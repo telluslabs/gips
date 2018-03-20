c4247125fb20cdf24604a51705124fea0f6402f3
Bobby H Braswell <rbraswell@ags.io>
2018-03-20 12:46:13 -0400

    don't autoremove

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 source credentials.sh
+
 wget -O sixs ftp://${AGSFTPCREDS}@agsftp.ags.io/gipsftp/sixs
-wget -O aod.composites.tgz ftp://${AGSFTPCREDS}@agsftp.ags.io/gipsftp/aod.composites.tar.gz
+wget -O composites.tgz ftp://${AGSFTPCREDS}@agsftp.ags.io/gipsftp/composites.tar.gz
 cp -f gips/settings_template.py gips/settings.py
 sed -i~ \
     -e "s/^EARTHDATA_USER.*/EARTHDATA_USER = \"${EARTHDATA_USER}\"/" \
@@ -10,8 +11,10 @@ sed -i~ \
     -e "s/^ESA_USER.*/ESA_USER = \"${ESA_USER}\"/" \
     -e "s/^ESA_PASS.*/ESA_PASS = \"${ESA_PASS}\"/" \
     gips/settings.py
+
 docker build -t gips --no-cache .
-rm -rf ${ARCHIVE}/*
-docker run --rm --name gips -h gips -v $ARCHIVE:/archive gips gips_config env
-tar xfvz aod.composites.tgz -C ${ARCHIVE}/
-docker run -it --rm --name gips -h gips -e GIPS_OVERRIDE_VERSION=0.8.2 -v /Users/braswell/data/gips:/archive gips
+
+rm -rf ${ARCHIVEDIR}
+docker run --rm --name gips -h gips -v ${ARCHIVEDIR}:/archive gips gips_config env
+tar xfvz aod.composites.tgz -C ${ARCHIVEDIR}/aod/
+docker run -it --rm --name gips -h gips -e GIPS_OVERRIDE_VERSION=0.8.2 -v ${ARCHIVEDIR}:/archive gips
