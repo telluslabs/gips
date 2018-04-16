ce5b5abab94624d10ae69ada7a30fdd5548219c2
Ian Cooke <icooke@ags.io>
2018-04-16 15:46:23 +0000

    Add gips_export

--- gips/scripts/project.py
+++ gips/scripts/project.py
@@ -21,70 +21,22 @@
 #   along with this program. If not, see <http://www.gnu.org/licenses/>
 ################################################################################
 
-import os
-from gips import __version__
-from gips.parsers import GIPSParser
-from gips.core import SpatialExtent, TemporalExtent
-from gips.utils import Colors, VerboseOut, import_data_class
-from gips import utils
-from gips.inventory import DataInventory, ProjectInventory
-from gips.inventory import orm
-
+import sys
+from gips.utils import verbose_out
+from . import export
 
 def main():
-    title = Colors.BOLD + 'GIPS Data Project (v%s)' % __version__ + Colors.OFF
-
-    # argument parsing
-    parser0 = GIPSParser(description=title)
-    parser0.add_inventory_parser(site_required=True)
-    parser0.add_process_parser()
-    parser0.add_project_parser()
-    parser0.add_warp_parser()
-    args = parser0.parse_args()
-
-    cls = utils.gips_script_setup(args.command, args.stop_on_error)
-    print title
-
-    with utils.error_handler():
-        extents = SpatialExtent.factory(
-            cls, site=args.site, rastermask=args.rastermask,
-            key=args.key, where=args.where, tiles=args.tiles,
-            pcov=args.pcov, ptile=args.ptile
-        )
-
-        # create tld: SITENAME--KEY_DATATYPE_SUFFIX
-        if args.notld:
-            tld = args.outdir
-        else:
-            key = '' if args.key == '' else '--' + args.key
-            suffix = '' if args.suffix == '' else '_' + args.suffix
-            res = '' if args.res is None else '_%sx%s' % (args.res[0], args.res[1])
-            bname = (
-                extents[0].site.LayerName() +
-                key + res + '_' + args.command + suffix
-            )
-            tld = os.path.join(args.outdir, bname)
-
-        for extent in extents:
-            t_extent = TemporalExtent(args.dates, args.days)
-            inv = DataInventory(cls, extent, t_extent, **vars(args))
-            datadir = os.path.join(tld, extent.site.Value())
-            if inv.numfiles > 0:
-                inv.mosaic(
-                    datadir=datadir, tree=args.tree, overwrite=args.overwrite,
-                    res=args.res, interpolation=args.interpolation,
-                    crop=args.crop, alltouch=args.alltouch,
-                )
-                inv = ProjectInventory(datadir)
-                inv.pprint()
-            else:
-                VerboseOut(
-                    'No data found for {} within temporal extent {}'
-                    .format(str(t_extent), str(t_extent)),
-                    2,
-                )
-
-    utils.gips_exit() # produce a summary error report then quit with a proper exit status
+    verbose_out(
+        "\n\n\n    DEPRECATION WARNING\n"
+        "   ----------------------\n"
+        "   'gips_project' has moved.\n"
+        "   It is now called 'gips_export',\n"
+        "   and 'gips_project' will be \n"
+        "   removed in a future release.\n\n\n",
+        0,
+        stream=sys.stderr,
+    )
+    export.main()
 
 
 if __name__ == "__main__":
