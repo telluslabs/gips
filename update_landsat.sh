a8f13cd3c71a46ba7ece422b7a70b7139a11eb12
Bobby H. Braswell <rob@telluslabs.com>
2018-05-24 18:45:34 +0000

    update landsat

---
+++ update_landsat.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+# daily update of Landsat for a specific place
+
+ARGS=("$@")
+OUTNAME=${args[0]}
+INSHPPATH="/archive/vector/"${args[1]}
+OUTSHPPATH="/archive/vector/"${args[2]}
+
+# the date is always yesterday
+DATE=$(date -d "yesterday" '+%Y-%m-%d')
+
+# the outdir from container perspective
+OUTDIR="/archive/"${OUTNAME}${DATE}
+
+# fetch and process reflectance and cloud mask
+gips_process landsat -p ref fmask -s ${SHPPATH} -v4 -d ${DATE} --fetch
+
+# export feature rasters for ARD tiles
+gips_export landsat -p ref fmask -s ${OUTSHPPATH} -v4 -d ${DATE} --outdir ${OUTDIR} --notld --res 30 30
+
+# apply cloud mask to reflectance images
+gips_mask ${OUTDIR}/* --pmask fmask
+
+# hackily split out separate bands
+gips_split ${OUTDIR}/* --prodname ref-masked
