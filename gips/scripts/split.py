f2fa5784b52b96f612444c2ab7596c5560342df8
Bobby H. Braswell <rob@telluslabs.com>
2018-05-25 15:24:25 +0000

    use gdal only - might need to roll back

--- gips/scripts/split.py
+++ gips/scripts/split.py
@@ -3,12 +3,37 @@
 import os
 
 import gippy
+
+import numpy as np
+from osgeo import gdal
+
 from gips.parsers import GIPSParser
 from gips.inventory import ProjectInventory
 from gips.utils import Colors, VerboseOut, basename
 from gips import utils
 
 from pdb import set_trace
+
+
+def read_raster(infile):
+    fp = gdal.Open(infile)
+    proj = fp.GetProjection()
+    geo = fp.GetGeoTransform()
+    data = fp.ReadAsArray()
+    ny, nx = (fp.RasterYSize, fp.RasterXSize)
+    return data, proj, geo
+
+
+def write_raster(outfile, outdata, proj, geo, nodata, dtype):
+    driver = gdal.GetDriverByName('GTiff')
+    ny, nx = outdata.shape
+    tfh = driver.Create(outfile, nx, ny, 1, dtype, [])
+    tfh.SetProjection(proj)
+    tfh.SetGeoTransform(geo)
+    tband = tfh.GetRasterBand(1)
+    tband.SetNoDataValue(nodata)    
+    tband.WriteArray(outdata)
+    tfh = None
 
 
 __version__ = '0.1.0'
@@ -21,18 +46,7 @@ def main():
     
     group = parser.add_argument_group('splitting options')
     group.add_argument('--prodname', help='Pattern of the target images')
-
-    #group.add_argument('--filemask', help='Mask all files with this static mask', default=None)
-    #group.add_argument('--pmask', help='Mask files with this corresponding product', nargs='*', default=[])
-    #h = 'Write mask to original image instead of creating new image'
-    #group.add_argument('--original', help=h, default=False, action='store_true')
-    #h = 'Overwrite existing files when creating new'
-    #group.add_argument('--overwrite', help=h, default=False, action='store_true')
-    #h = 'Suffix to apply to masked file (not compatible with --original)'
-    #group.add_argument('--suffix', help=h, default='-masked')
     args = parser.parse_args()
-
-    # TODO - check that at least 1 of filemask or pmask is supplied
 
     utils.gips_script_setup(None, args.stop_on_error)
 
@@ -40,18 +54,10 @@ def main():
 
         VerboseOut(title)
         for projdir in args.projdir:
-    
-            #if args.filemask is not None:
-            #    mask_file = gippy.GeoImage(args.filemask)
 
             inv = ProjectInventory(projdir, args.products)
             for date in inv.dates:
                 VerboseOut('Splitting files from %s' % date)
-                
-                #if args.filemask is None and args.pmask == []:
-                #    available_masks = inv[date].masks()
-                #else:
-                #    available_masks = inv[date].masks(args.pmask)
 
                 for p in inv.products(date):
 
@@ -62,46 +68,43 @@ def main():
 
                     if not fname.endswith("{}.tif".format(args.prodname)):
                         continue
-                    
+
                     bnames = img.BandNames()
-                    
+
+                    imgdata, proj, geo = read_raster(fname)
+                        
                     for i,bname in enumerate(bnames):
 
                         fnameout = "{}_{}.tif".format(
                             os.path.splitext(fname)[0], bname)
 
-                        data = img[i].Read()
+                        #data = img[i].Read()
+
+                        data = imgdata[i,:,:].squeeze()
                         
-                        imgout = gippy.GeoImage(fnameout, img, gippy.GDT_Int16, 1)
-                        imgout.SetNoData(-32768)
-                        imgout.SetOffset(0.0)
-                        imgout.SetGain(0.0001)
-                        imgout[0].Write(data)
-                        imgout.SetBandName(bname, 1)
-                        imgout = None
+                        nodata = -32768
+                        scale = 10000
+                        notmissing = np.where(data!=nodata)
+                        data[notmissing] = data[notmissing]*scale
+                        data = data.astype('int16')
+                        dtype = gdal.GDT_Int16
+                        
+                        write_raster(fnameout, data, proj, geo, nodata, dtype)
+
+                        set_trace()
+                        
+                        #data[notmissing] = data[]
+                        # do gdal stuff here
+                        
+                        #imgout = gippy.GeoImage(fnameout, img, gippy.GDT_Int16, 1)
+                        #imgout.SetNoData(-32768)
+                        #imgout.SetOffset(0.0)
+                        #imgout.SetGain(0.0001)
+                        #imgout[0].Write(data)
+                        #imgout.SetBandName(bname, 1)
+                        #imgout = None
                         
                     img = None
-    
-                    #if args.filemask is not None:
-                    #    img.AddMask(mask_file[0])
-                    #    meta = basename(args.filemask) + ' '
-                    #for mask in available_masks:
-                    #    img.AddMask(inv[date].open(mask)[0])
-                    #    meta = meta + basename(inv[date][mask]) + ' '
-                    #if meta != '':
-                    #    if args.original:
-                    #        VerboseOut('  %s' % (img.Basename()), 2)
-                    #        img.Process()
-                    #        img.SetMeta('MASKS', meta)
-                    #    else:
-                    #        fout = os.path.splitext(img.Filename())[0] + args.suffix + '.tif'
-                    #        if not os.path.exists(fout) or args.overwrite:
-                    #            VerboseOut('  %s -> %s' % (img.Basename(), basename(fout)), 2)
-                    #            imgout = img.Process(fout)
-                    #            imgout.SetMeta('MASKS', meta)
-                    #            imgout = None
-                    #img = None
-            #mask_file = None
 
     utils.gips_exit()
 
