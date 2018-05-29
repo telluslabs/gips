96e7675a955ce0dcb143d78ff32cf99337384344
Bobby H. Braswell <rob@telluslabs.com>
2018-05-29 15:15:30 -0400

    update print statement

--- update_landsat.sh
+++ update_landsat.sh
@@ -17,7 +17,7 @@ echo "gips_process landsat -p ref fmask -s ${INSHPPATH} -v4 -d ${DATE} --fetch"
 gips_process landsat -p ref fmask -s ${INSHPPATH} -v4 -d ${DATE} --fetch
 
 # export feature rasters for output tiles
-echo "gips_export landsat -p ref fmask -s ${OUTSHPPATH} -v4 -d ${DATE} --outdir ${OUTDIR} --notld --res 30 30"
+echo "gips_export landsat -p ref fmask -s ${OUTSHPPATH} -v4 -d ${DATE} --outdir ${OUTDIR} --notld --res 30 30 --key ${OUTSHPKEY}"
 gips_export landsat -p ref fmask -s ${OUTSHPPATH} -v4 -d ${DATE} --outdir ${OUTDIR} --notld --res 30 30 --key ${OUTSHPKEY}
 
 # apply cloud mask to reflectance images
