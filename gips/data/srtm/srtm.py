225d9437e1621b641b838716f7fe79327ae85b45
Nate Rubin <nrubin@appliedgeosolutions.com>
2018-06-01 16:22:15 -0400

    use USGS data pool instead of EE api

--- gips/data/srtm/srtm.py
+++ gips/data/srtm/srtm.py
@@ -1,6 +1,8 @@
 import datetime
 import re
 import os
+import subprocess
+from zipfile import ZipFile
 
 from gips.data.core import Repository, Asset, Data
 from gips import utils
@@ -24,7 +26,7 @@ class srtmAsset(Asset):
 
     _assets = {
         'srtm': {
-            'pattern': r'^n(?P<north>\d{2})_w(?P<west>\d{3})_1arc_v3_bil.zip$',
+            'pattern': r'^(?P<tile>[NS]\d{2}[WE]\d{3})_2000042_srtm_srtm.tif$',
             'startdate': datetime.date(2000, 2, 1),
             'latency': 0,
         },
@@ -41,55 +43,52 @@ class srtmAsset(Asset):
             msg = "No matching SRTM asset type for '{}'".format(fname)
             raise RuntimeError(msg, filename)
 
-        self.tile = "N{}W{}".format(match.group('north'), match.group('west'))
-
-    @classmethod
-    def ee_login(cls):
-        if not hasattr(cls, '_ee_key'):
-            username = settings().REPOS['srtm']['username']
-            password = settings().REPOS['srtm']['password']
-            from usgs import api
-            cls._ee_key = api.login(username, password)['data']
-        return cls._ee_key
+        self.tile = match.group('tile')
+        self.date = datetime.datetime(2000, 2, 11)
 
     @classmethod
     def query_service(cls, asset, tile, date):
-        utils.verbose_out("Querying tile %s" % tile, 3)
-
-        api_key = cls.ee_login()
-        print api_key
-        from usgs import api
-        response = api.search(
-            'SRTM_V3_SRTMGL1', 'EE',
-            api_key=api_key
-        )['data']
-
-        result = [r for r in response if r['displayId'].split('.')[0] == tile]
-        if len(result) > 0:
+        url_base = 'https://e4ftl01.cr.usgs.gov/MEASURES/SRTMGL1.003/2000.02.11/{}.SRTMGL1.hgt.zip'
+        if date == datetime.datetime(2000, 2, 11):
             return {
-                'display_id': result[0]['displayId'],
-                'download_url': result[0]['downloadUrl'],
+                'url': url_base.format(tile),
+                'basename': '{}_2000042_srtm_srtm.tif'.format(tile),
+                'download_name': '{}.SRTMGL1.hgt.zip'.format(tile),
             }
         return None
 
     @classmethod
     def fetch(cls, asset, tile, date):
-        print "FETCHING"
         qs_rv = cls.query_service(asset, tile, date)
         if qs_rv is None:
             return []
-        url = qs_rv.pop('download_url')
+        url = qs_rv.pop('url')
+        basename = qs_rv.pop('basename')
+        download_name = qs_rv.pop('download_name')
 
         stage_dir = cls.Repository.path('stage')
+        fetched = []
 
         with utils.make_temp_dir(prefix='dwnld', dir=stage_dir) as dldir:
-            homura.download(url, dldir)
-            granules = os.listdir(dldir)
-            if len(granules) == 0:
-                raise Exception("Download didn't seem to"
-                                " produce a file: {}".format(str(granules)))
-            os.rename(os.path.join(dldir, granules[0]),
-                      os.path.join(stage_dir, granules[0]))
+            zip_filename = os.path.join(dldir, download_name)
+            subprocess.call([
+                "wget",
+                "--no-verbose",
+                "--user", settings().REPOS['srtm']['username'],
+                "--password", settings().REPOS['srtm']['password'],
+                "--output-document", zip_filename,
+                url
+            ])
+
+            zip_file = ZipFile(zip_filename)
+            hgt_filename = zip_file.extract(zip_file.filelist[0], dldir)
+            tif_filename = os.path.join(dldir, basename)
+            subprocess.call(["gdal_translate", "-of", "GTiff", hgt_filename, tif_filename])
+
+            os.rename(tif_filename,
+                      os.path.join(stage_dir, os.path.basename(tif_filename)))
+            fetched.append(os.path.join(stage_dir, os.path.basename(tif_filename)))
+        return fetched
 
 
 class srtmData(Data):
