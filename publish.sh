f3ec118e0c813638e721e160a96889522a1f624f
Bobby H. Braswell <rob@telluslabs.com>
2018-07-16 07:02:27 -0400

    cleaning up unused files

--- publish.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-TAG=${1:-latest}
-
-docker login registry.gitlab.com
-docker build -t registry.gitlab.com/rbraswell/gips:${TAG} .
-docker push registry.gitlab.com/rbraswell/gips:${TAG}
