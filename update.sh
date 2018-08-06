4cb9617c04f716f9cc86d28d9e31d881c7510a64
Bobby H. Braswell <rob@telluslabs.com>
2018-08-06 19:57:20 -0400

    --overwrite on update.sh

--- update.sh
+++ update.sh
@@ -7,9 +7,9 @@ S3PREFIX=$4
 
 /bin/bash initialize.sh
 
-gips_process landsat -p ref temp fmask -s $SHPFILE -d $DATE -v4 --fetch
+gips_process landsat -p ref temp fmask -s $SHPFILE -d $DATE -v4 --fetch --overwrite
 
-gips_export landsat -p ref temp fmask -s $SHPFILE -d $DATE -v4 --outdir $OUTDIR --notld --res 30 30 --key tileid
+gips_export landsat -p ref temp fmask -s $SHPFILE -d $DATE -v4 --outdir $OUTDIR --notld --res 30 30 --key tileid --overwrite
 
 gips_mask ${OUTDIR}/* --pmask fmask
 
