53b06dfbc9e48c9275280d389159874f7bd11573
Tom Olson <tolson@appliedgeosolutions.com>
2018-12-10 13:13:08 -0500

    605 more bones for the hls driver

--- gips/data/hls/hls.py
+++ gips/data/hls/hls.py
@@ -20,6 +20,8 @@
 
 from __future__ import print_function
 
+from backports.functools_lru_cache import lru_cache
+
 from gips.data.core import Repository, Asset, Data
 
 
@@ -32,6 +34,53 @@ class hlsRepository(Repository):
 class hlsAsset(Asset):
     Repository = hlsRepository
 
+    _sensors = {} # TODO
+
+    _assets = {} # TODO
+
+    def __init__(self):
+        super(hlsAsset, self).__init__(filename)
+
+    @classmethod
+    @lru_cache(maxsize=100) # cache size chosen arbitrarily
+    def query_service(cls, asset, tile, date, pclouds=100, **ignored):
+        # TODO query_provider instead?
+        raise NotImplementedError()
+
+    @classmethod
+    def fetch(cls, asset, tile, date, pclouds=100, **ignored):
+        raise NotImplementedError()
+
 
 class hlsData(Data):
+    version = '1.0.0'
     Asset = hlsAsset
+
+    _productgroups = {} # TODO
+
+    _products = {} # TODO
+
+    @classmethod
+    def normalize_tile_string(cls, tile_string):
+        # TODO probably copy sentinel-2
+        return tile_string
+
+    @classmethod
+    def add_filter_args(cls, parser):
+        # TODO probably copy sentinel-2
+        pass
+
+    def filter(self, pclouds=100, **kwargs):
+        # TODO confirm this use of sentinel-2 version
+        return all([asset.filter(pclouds, **kwargs) for asset in self.assets.values()])
+
+    def prep_meta(self, additional=None):
+        # TODO confirm this use of sentinel-2 version
+        meta = super(sentinel2Data, self).prep_meta(
+            self.current_asset().filename, additional)
+        return meta
+
+    @Data.proc_temp_dir_manager
+    def process(self, products=None, overwrite=False, **kwargs):
+        # TODO for now just gippy indices
+        raise NotImplementedError()
