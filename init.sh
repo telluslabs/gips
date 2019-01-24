127b464839804cc6481e000eaa7dd9501c489e92
Bobby H. Braswell <rob@telluslabs.com>
2019-01-24 13:47:58 -0500

    init env vars

---
+++ init.sh
@@ -0,0 +1,3 @@
+export GIPS_S3="" # where to obtain gips_init.tgz on S3 (for initialization)
+export GIPS_DATA="" # local dir maps to /export in container (for sharing data)
+export GIPS_HOME="" # local dir maps to /gips in container (for development)
