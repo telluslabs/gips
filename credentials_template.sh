558fd465bb77b5b24fdff08fb90effecd3e8431e
Bobby H. Braswell <rob@telluslabs.com>
2019-01-27 11:58:50 -0500

    clean up setup files

--- credentials_template.sh
+++
@@ -1,8 +0,0 @@
-ARCHIVEDIR="" # path to shared gips archive
-AGSFTPCREDS="" # ftp credentials for sixs and aod
-EARTHDATA_USER="" # Earthdata credentials (see https://earthdata.nasa.gov)
-EARTHDATA_PASS="" # ... for MODIS, MERRA, ASTER
-USGS_USER="" # USGS credentials (see https://earthexplorer.usgs.gov)
-USGS_PASS="" # ... for Landsat
-ESA_USER="" # ESA credentials (see https://scihub.copernicus.eu)
-ESA_PASS="" # ... for Sentinel2 and Sentinel1
--- init_template.sh
+++
@@ -1,3 +0,0 @@
-export GIPS_S3="" # where to obtain gips_init.tgz on S3 (for initialization)
-export GIPS_DATA="" # local dir maps to /export in container (for sharing data)
-export GIPS_HOME="" # local dir maps to /gips in container (for development)
