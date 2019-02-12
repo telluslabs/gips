4f1e0f4f5dc02896aa31c21d05ccb8b68ebc3cae
Bobby H. Braswell <rob@telluslabs.com>
2019-02-12 06:58:35 -0500

    gips_tiles might work

--- gips/src/gips_tiles.py
+++ gips/src/gips_tiles.py
@@ -10,6 +10,7 @@ from __future__ import print_function
 import os
 import click
 
+from tempfile import NamedTemporaryFile
 from backports import tempfile
 
 from make_global_tiles import make_tileimg
@@ -25,12 +26,51 @@ S3LOC = "s3://tellus-s3-vault/product/analysis_tiles/{product}_{window}/{year}/{
 
 MASKFILE = "{dirname}/{tileid}_1_1.tif"
 
+
+PROJDIR = "/export/{asset}_{product}_ks"
+
 GIPS_CMD = "gips_export {asset} -p {product} -r {maskfile} -d {year} "\
-"--days {dayrange} -v4 --outdir /export/{asset}_{product}_ks --notld --fetch --overwrite > {logfile}"
+"--days {dayrange} -v4 --outdir {projdir} --notld --fetch --overwrite > {logfile}"
 
 AWS_CMD = "aws s3 cp {} {} > {}"
 
 LOCAL_VOLUME = '/tmp'
+
+
+MULTITEMPORAL_CMD = "multitemporal --conf {conffile} --nproc 1 > {logfile}"
+
+
+CONF_DATA = """{{
+    "projname" : "window",
+    "projdir"  : "{projdir}",
+    "outdir"   : "{projdir}_composite",
+    "dperframe": 1,
+    "nproc"    : 1,
+    "sources":
+    [
+        {{
+            "name": "{varname}",
+            "regexp": "^(\\\d{{7}})_\\\w{{3}}_{varname}-masked.tif$",
+            "bandnum": 1
+        }}
+    ],
+    "steps":
+    [
+        {{
+            "module" : "interpolate",
+            "params" : [],
+            "inputs" : "{varname}",
+            "output" : false
+        }},
+        {{
+            "module" : "window",
+            "params" : [{dayrange}],
+            "inputs" : "interpolate",
+            "output" : true
+        }}
+    ]
+}}"""
+
 
 
 def temporary_directory():
@@ -41,68 +81,77 @@ def temporary_directory():
 @click.command()
 @click.option('-a', '--asset', required=True, help='')
 @click.option('-p', '--product', required=True, help='')
-@click.option('-y', '--years', required=True, help='')
+@click.option('-y', '--year', required=True, help='')
 @click.option('-w', '--window', help='')
 @click.option('-d', '--dayrange', help='')
-def main(asset, product, years, window, dayrange):
+def main(asset, product, year, window, dayrange):
 
-	h0 = 519
-	v0 = 333
+    h0 = 519
+    v0 = 333
 
     if asset == "cdl":
-    	if window is None:
-        	window = "all"
+        if window is None:
+            window = "all"
         else:
-        	assert window == "all"
+            assert window == "all"
         if dayrange is None:
             dayrange = "1,1"
         else:
-        	assert dayrange = "1,1"
+            assert dayrange == "1,1"
 
-	years = years.split(',')
-	years = [int(y) for y in years]
+    for i in range(51):
+        for j in range(21):
 
-	for year in years:
+            htile = h0 + i
+            vtile = v0 + j
 
-		for i in range(51):
-			for j in range(21):
+            tileid = '{}_{}'.format(htile, vtile)
 
-				htile = h0 + i
-				vtile = v0 + j
+            with temporary_directory() as tmp_dir:
 
-				tileid = '{}_{}'.format(htile, vtile)
+                print('make_tileimg', tileid)
+                make_tileimg(tmp_dir, tileid, '1,1')
 
-				with temporary_directory() as tmp_dir:
+                maskfile = MASKFILE.format(dirname=tmp_dir, tileid=tileid)
 
-					print('make_tileimg', tileid)
-					make_tileimg(tmp_dir, tileid, '1,1')
+                projdir = PROJDIR.format(asset=asset, product=product)
 
-					maskfile = MASKFILE.format(dirname=tmp_dir, tileid=tileid)
+                # TODO: don't you like how I avoided using subprocess, sh, or commands?
+                gips_cmd = GIPS_CMD.format(asset=asset, product=product, maskfile=maskfile,
+                    year=year, dayrange=dayrange, logfile=LOGFILE, projdir=projdir)
 
-					# TODO: don't you like how I avoided using subprocess, sh, or commands?
-					gips_cmd = GIPS_CMD.format(asset=asset, product=product, maskfile=maskfile,
-						year=year, dayrange=dayrange, logfile=LOGFILE)
+                print(gips_cmd)
+                os.system(gips_cmd)
+                print(open(LOGFILE).read())
+                os.remove(LOGFILE)
 
-					print(gips_cmd)
-					os.system(gips_cmd)
-					print(open(LOGFILE).read())
-					os.remove(LOGFILE)
+                productfile = PRODUCTFILE.format(asset=asset, product=product, year=year)
+                s3loc = S3LOC.format(product=product, window=window, year=year, tileid=tileid)
 
-					productfile = PRODUCTFILE.format(asset=asset, product=product, year=year)
-					s3loc = S3LOC.format(product=product, window=window, year=year, tileid=tileid)
+                aws_cmd = AWS_CMD.format(productfile, s3loc, LOGFILE)
+                print(aws_cmd)
+                os.system(aws_cmd)
+                print(open(LOGFILE).read())
+                os.remove(LOGFILE)
 
-					aws_cmd = AWS_CMD.format(productfile, s3loc, LOGFILE)
-					print(aws_cmd)
-					os.system(aws_cmd)
-					print(open(LOGFILE).read())
-					os.remove(LOGFILE)
+                print('removing', productfile)
+                os.remove(productfile)
 
-					print('removing', productfile)
-					os.remove(productfile)
+                print('removing', maskfile)
+                os.remove(maskfile)
 
-					print('removing', maskfile)
-					os.remove(maskfile)
+                if window != "all":
+                    with NamedTemporaryFile() as tmpfile:
+                        conf_data = CONF_DATA.format(projdir=projdir, varname=product, dayrange=dayrange)
+                        tmpfile.write(conf_data)
+
+                        multitemporal_cmd = MULTITEMPORAL_CMD.format(conffile=tmpfile.name, logfile=LOGFILE)
+                        print(multitemporal_cmd)
+                        os.system(multitemporal_cmd)
+                        print(open(LOGFILE).read())
+                        os.remove(LOGFILE)
+
 
 
 if __name__ == "__main__":
-	main()
+    main()
