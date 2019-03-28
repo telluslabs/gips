9a7f8783b4c93326020a75f1e022b9e5c44a7b69
Bobby H. Braswell <rob@telluslabs.com>
2019-03-28 08:41:58 -0400

    cdl tiles loops over years

--- gips/src/cdl_tiles.py
+++ gips/src/cdl_tiles.py
@@ -1,12 +1,14 @@
 from __future__ import print_function
 
 import os
+from copy import deepcopy
+
 import click
+import boto3
+import geopandas as gpd
 
 from backports import tempfile
 # from tempfile import TemporaryDirectory
-
-import geopandas as gpd
 
 from make_global_tiles import make_tileimg
 
@@ -17,6 +19,8 @@ TMPFILE = "/tmp/output.txt"
 
 PRODUCTFILE = "/export/{asset}_{product}/0/{year}001_{asset}_{product}.tif"
 
+
+S3_BUCKET = "tellus-s3-vault"
 S3LOC = "s3://tellus-s3-vault/product/analysis_tiles/cdl_all/{}/{}.tif"
 
 MASKFILE = "{}/{}_1_1.tif"
@@ -26,49 +30,79 @@ GIPS_CMD = "gips_export {asset} -p {product} -r {maskfile} -d {year} "\
 
 AWS_CMD = "aws s3 cp {} {} > {}"
 
+AWS_LS_CMD = "aws s3 ls {} > {}"
+
 LOCAL_VOLUME = '/tmp'
 
 
-def run(shpfile, year):
 
-	asset = "cdl"
-	product = "cdl"
+def get_tileids(shpfile):
 
-	df = gpd.read_file(shpfile)
+    df = gpd.read_file(shpfile)
 
-	tileids =  [str(t) for t in df['tileid'].tolist()]
+    tileids =  [str(t) for t in df['tileid'].tolist()]
 
-	for tileid in tileids:
+    print(len(tileids))
 
-		htile, vtile = tileid.split('_')
+    for tileid in deepcopy(tileids):
+        htile, vtile = tileid.split('_')
+        for i in [-1,0,1]:
+            for j in [-1,0,1]:
+                newtileid = "{}_{}".format(int(htile)+i, int(vtile)+j)
+                if newtileid not in tileids:
+                    tileids.append(newtileid)
 
-		with tempfile.TemporaryDirectory() as tmp_dir:
-		# with temporary_directory() as tmp_dir:
+    print(len(tileids))
 
-			print('make_tileimg', tileid)
-			make_tileimg(tmp_dir, tileid, '1,1')
+    return sorted(tileids)
 
-			maskfile = MASKFILE.format(tmp_dir, tileid)
 
-			gips_cmd = GIPS_CMD.format(asset=asset, product=product, maskfile=maskfile, year=year, tmpfile=TMPFILE)
-			print(gips_cmd)
-			os.system(gips_cmd)
-			print(open(TMPFILE).read())
+def run(tileids, year):
 
-			productfile = PRODUCTFILE.format(asset=asset, product=product, year=year)
-			s3loc = S3LOC.format(year, tileid)
+    asset = "cdl"
+    product = "cdl"
 
-			aws_cmd = AWS_CMD.format(productfile, s3loc, TMPFILE)
+    for tileid in tileids:
 
-			print(aws_cmd)
-			os.system(aws_cmd)
-			print(open(TMPFILE).read())
+        htile, vtile = tileid.split('_')
 
-			print('removing', productfile)
-			os.remove(productfile)
+        with tempfile.TemporaryDirectory() as tmp_dir:
 
-			print('removing', maskfile)
-			os.remove(maskfile)
+            productfile = PRODUCTFILE.format(asset=asset, product=product, year=year)
+            s3loc = S3LOC.format(year, tileid)
+
+            aws_cmd = AWS_LS_CMD.format(s3loc, TMPFILE)
+
+            print(aws_cmd)
+            os.system(aws_cmd)
+
+            result = open(TMPFILE).read()
+
+            if tileid+'.tif' in result:
+                print('have', tileid)
+                continue
+
+            print('make_tileimg', tileid)
+            make_tileimg(tmp_dir, tileid, '1,1')
+
+            maskfile = MASKFILE.format(tmp_dir, tileid)
+
+            gips_cmd = GIPS_CMD.format(asset=asset, product=product, maskfile=maskfile, year=year, tmpfile=TMPFILE)
+            print(gips_cmd)
+            os.system(gips_cmd)
+            print(open(TMPFILE).read())
+
+            aws_cmd = AWS_CMD.format(productfile, s3loc, TMPFILE)
+
+            print(aws_cmd)
+            os.system(aws_cmd)
+            print(open(TMPFILE).read())
+
+            print('removing', productfile)
+            os.remove(productfile)
+
+            print('removing', maskfile)
+            os.remove(maskfile)
 
 
 @click.command()
@@ -78,10 +112,12 @@ def main(shapefile, years):
 
     # e.g. python cdl_tiles.py -s /export/kansas/kansas_tiles.shp -y 2008,2009,2010,2011,2012,2013,2014,2015,2016,2017,2018
 
+    tileids = get_tileids(shapefile)
     years = [int(y) for y in years.split(',')]
+
     for year in years:
-        run(shpfile, year)
+        run(tileids, year)
 
 
 if __name__ == "__main__":
-	main()
+    main()
