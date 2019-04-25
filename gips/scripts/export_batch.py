ba677749a51a6529124ad585bf79110d5759c358
Bobby H. Braswell <rob@telluslabs.com>
2019-04-25 06:58:14 -0400

    export_batch parallelize by day

--- gips/scripts/export_batch.py
+++ gips/scripts/export_batch.py
@@ -34,15 +34,14 @@ from gips.inventory import orm
 
 from gips.scripts.export import run_export
 
-
 import click
 import glob
-# import boto3
-# import zipfile
 import shutil
+import ogr
 
-
-from pdb import set_trace
+from backports import tempfile
+import boto3
+import zipfile
 
 
 class Args(object):
@@ -50,59 +49,83 @@ class Args(object):
 
 
 @click.command()
+@click.option('--name', '-n', type=str, help='Job name')
 @click.option('--jobid', '-j', type=int, help='Job index')
-def main(jobid):
+def main(name, jobid):
 
     title = Colors.BOLD + 'GIPS Data Export (v%s)' % __version__ + Colors.OFF
     print(title)
 
-    with utils.error_handler():
-
-        args = Args()
-        args.command = "hls"
-        args.products = ['ndvi', 'lswi', 'brgt', 'cmask']
-        args.res = [30., 30.]
-        args.stop_on_error = "False"
-        args.suffix = ""
-        args.format = "GTiff"
-        args.verbose = 4
-        args.interpolation = 0
-        args.ptile = 0
-        args.pcov = 0
-        args.pclouds = 100.
-        args.chunksize = 128.0
-        args.numprocs = 1
-        args.notld = True
-        args.fetch = True
-        args.crop = False
-        args.overwrite = False
-        args.alltouch = False
-        args.tree = False
-        args.size = False
-        args.update = False
-        args.days = None
-        args.rastermask = None
-        args.tiles = None
-        args.batchout = None
-        args.sensors = None
-
-        args.site = "s3://tl-octopus/user/gips/vector/ag_phenocam_tiles_5070.zip"
-        args.key = "tileid"
-
-        doy = jobid % 365
-        fid = jobid / 365
-
-        # args.outdir = "/archive/export/ag_phenocam_tiles_{}_{}_{}".format(jobid, doy, fid)
-        args.outdir = "s3://rob-scratch/gips/export/ag_phenocam_tiles_{}_{}".format(doy, fid)
+    args = Args()
+    args.command = "hls"
+    args.products = ['ndvi', 'lswi', 'brgt', 'cmask']
+    args.res = [30., 30.]
+    args.stop_on_error = "False"
+    args.suffix = ""
+    args.format = "GTiff"
+    args.verbose = 4
+    args.interpolation = 0
+    args.ptile = 0
+    args.pcov = 0
+    args.pclouds = 100.
+    args.chunksize = 128.0
+    args.numprocs = 1
+    args.notld = True
+    args.fetch = True
+    args.crop = False
+    args.overwrite = False
+    args.alltouch = False
+    args.tree = False
+    args.size = False
+    args.update = False
+    args.days = None
+    args.rastermask = None
+    args.tiles = None
+    args.batchout = None
+    args.sensors = None
+    args.key = ''
 
 
-        args.dates = "2017-{}".format(str(doy+1).zfill(3))
-        args.where = "FID={}".format(fid)
+    with tempfile.TemporaryDirectory() as tmpdir:
 
-        print(args.dates)
-        print(args.where)
+        args.site = "s3://tl-octopus/user/gips/vector/{}.zip".format(name)
 
-        run_export(args)
+        # get the number of features TLDR
+        S3 = boto3.resource('s3')
+        s3path = args.site.lstrip('s3://')
+        filename = s3path.split('/')[-1]
+        s3_bucket = s3path.split('/')[0]
+        s3_key = "/".join(s3path.split('/')[1:])
+        zippath = os.path.join(tmpdir, filename)
+        S3.Bucket(s3_bucket).download_file(s3_key, zippath)
+        zipped = zipfile.ZipFile(zippath)
+        zipped.extractall(tmpdir)
+        shppath = os.path.splitext(zippath)[0] + '.shp'
+        driver = ogr.GetDriverByName('ESRI Shapefile')
+        source = driver.Open(shppath, 0)
+        layer = source.GetLayer()
+
+        nfeatures = layer.GetFeatureCount()
+        source.Destroy()
+
+        doy = jobid
+
+        for fid in range(nfeatures):
+
+            print(fid)
+
+            args.site = "s3://tl-octopus/user/gips/vector/{}.zip".format(name)
+            args.outdir = "s3://tl-octopus/user/gips/export/{}/{}_{}_{}".format(name, doy, fid)
+
+            args.dates = "2017-{}".format(str(doy+1).zfill(3))
+            args.where = "FID={}".format(fid)
+
+            print(args.dates)
+            print(args.where)
+
+            run_export(args)
+
+            print('done export')
 
         print('cleaning up')
 
