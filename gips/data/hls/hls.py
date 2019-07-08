d49cc75fcce3b53886e1de5415e3f6119fc42531
Bobby H. Braswell <rob@telluslabs.com>
2019-07-08 07:32:37 -0400

    further testing revealed a couple bugs- fixed

--- gips/data/hls/hls.py
+++ gips/data/hls/hls.py
@@ -36,6 +36,9 @@ from gips.data.sentinel2 import sentinel2
 from gips.data.landsat import landsat
 
 
+from pdb import set_trace
+
+
 # User guide & other docs here:  https://hls.gsfc.nasa.gov/documents/
 
 _hls_version = '1.4'
@@ -163,14 +166,17 @@ class hlsAsset(gips.data.core.CloudCoverAsset,
         url = '/'.join([_url_base, asset, str(date.year), zbcr, basename])
         if requests.head(url).status_code == 200: # so do they have it?
             return basename, url
-        return None, None
+        return None
 
     _s3_bucket_name = "hlsp1"
     _s3_base_key = "HLS_LOCAL_IO.v1.4"
 
     @classmethod
     def query_s3(cls, asset, tile, date, **ignored):
-        cls.check_hls_version()
+
+        # TODO: check version in S3 mode
+        #cls.check_hls_version()
+
         # build the full URL & basename of the file
         basename = 'HLS.{}.T{}.{}.v{}.hdf'.format(
             asset, tile, date.strftime('%Y%j'), _hls_version)
@@ -195,7 +201,7 @@ class hlsAsset(gips.data.core.CloudCoverAsset,
 
         if len(s30keys) > 0:
             key = [k for k in s30keys if '.hdf' in k][0]
-        elif len(l30keys > 0):
+        elif len(l30keys) > 0:
             key = [k for k in l30keys if '.hdf' in k][0]
         else:
             return None
@@ -210,10 +216,14 @@ class hlsAsset(gips.data.core.CloudCoverAsset,
         if not cls.available(asset, date):
             return None
         if cls.data_src() == 'http':
-            bn, url = cls.query_http(asset, tile, date)
+            result = cls.query_http(asset, tile, date)
         else:
-            bn, url = cls.query_s3(asset, tile, date)
-        return {'basename': bn, 'url': url}
+            result = cls.query_s3(asset, tile, date)
+        # caller needs either None or this dictionary
+        if result is not None:
+            bn, url = result
+            result = {'basename': bn, 'url': url}
+        return result
 
     @classmethod
     def download_http(cls, url, download_fp, pclouds=100.0):
