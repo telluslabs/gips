053352557c68436329dd6b345442ac9dbd042c75
Subit <subit@telluslabs.com>
2019-09-25 14:32:45 -0400

    Changed memory

--- gips/data/sentinel1/sentinel1.py
+++ gips/data/sentinel1/sentinel1.py
@@ -317,7 +317,7 @@ class sentinel1Asset(Asset):
                 targetfile = os.path.join(downloaddir, scene + '.tif')
                 print('processing', sourcefile, targetfile)
 
-                cmd = "/usr/bin/gpt {} -e -SsourceProduct={} -PtargetProduct={} -xmx32g " \
+                cmd = "/usr/bin/gpt {} -J-Xmx32G -J-Xms12G -q 8 -e -SsourceProduct={} -PtargetProduct={} " \
                       "-Dsnap.dataio.bigtiff.compression.type=LZW".format(graph_xml_path, sourcefile, targetfile)
                 print(cmd)
                 command(cmd)
